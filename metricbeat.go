126591e8dab30f48d21c78472d82844ef6fce485
Manuel de la Peña <mdelapenya@gmail.com>
2019-07-19 12:47:31 +0200

    Inspect the monitored service to use its IPAddress and passit to the metricbeat configuration as an env var

--- metricbeat.go
+++ metricbeat.go
@@ -1,22 +1,37 @@
 package main
 
-import "os"
+import (
+	"os"
+	"fmt"
+	
+	docker "github.com/elastic/metricbeat-tests-poc/docker"
+)
 
 // NewMetricbeatService returns a metricbeat service entity
 func NewMetricbeatService(version string, monitoredService Service) Service {
 	dir, _ := os.Getwd()
 
+	serviceName := monitoredService.GetName()
+
+	inspect, err := docker.InspectContainer(serviceName)
+	if err != nil {
+		fmt.Errorf("Could not inspect service %s", serviceName)
+		return nil
+	}
+
+	ip := inspect.NetworkSettings.IPAddress
+	fmt.Printf("The monitored service (%s) runs on %s\n", serviceName, ip)
 	env := map[string]string{
-		"HOST": "localhost",
+		"HOST": ip,
 	}
 
 	bindMounts := map[string]string{
-		dir + "/configs/" + monitoredService.GetName() + ".yml": "/usr/share/metricbeat/metricbeat.yml",
+		dir + "/configs/" + serviceName + ".yml": "/usr/share/metricbeat/metricbeat.yml",
 		dir + "/outputs": "/tmp",
 	}
 
 	labels := map[string]string{
-		"co.elastic.logs/module": monitoredService.GetName(),
+		"co.elastic.logs/module": serviceName,
 	}
 
 	return &DockerService{
