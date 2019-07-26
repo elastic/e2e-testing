f38134df2fee426ed2ec98595d9ba6ed2fd86b06
Manuel de la Peña <mdelapenya@gmail.com>
2019-07-26 11:28:50 +0200

    Fix unit tests for services

--- services/service_test.go
+++ services/service_test.go
@@ -3,23 +3,24 @@ package services
 import (
 	"testing"
 
+	services "github.com/elastic/metricbeat-tests-poc/services"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewService_ContainerNameIncludesVersionAsSuffix(t *testing.T) {
-	apache := NewApacheService("2.2", false)
+	apache := services.NewApacheService("2.2", false)
 
 	assert.Equal(t, "apache-2.2", apache.GetContainerName())
 }
 
 func TestNewService_ExposedPortsIsEmpty(t *testing.T) {
-	apache := NewApacheService("2.2", false)
+	apache := services.NewApacheService("2.2", false)
 
-	assert.Equal(t, 80, apache.GetExposedPort())
+	assert.Equal(t, "80", apache.GetExposedPort())
 }
 
 func TestNewService_Name(t *testing.T) {
-	apache := NewApacheService("2.2", false)
+	apache := services.NewApacheService("2.2", false)
 
 	assert.Equal(t, "apache", apache.GetName())
 }
