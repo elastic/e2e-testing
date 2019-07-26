e2fdf364e159d6eeb14aa4b25b1f33cc22f7bb71
Manuel de la Peña <mdelapenya@gmail.com>
2019-07-26 02:19:40 +0200

    Rename CLI to eod (Elastic Observability Dev)

--- cmd/root.go
+++ cmd/root.go
@@ -8,9 +8,9 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "mt",
-	Short: "mt (Metricbeat tests) makes it easier to test Metricbeat's modules.",
-	Long: `A Fast and Flexible CLI for testing Metricbeat's modules
+	Use:   "eod",
+	Short: "eod (Elastic Observability Dev) makes it easier to develop Observability projects.",
+	Long: `A Fast and Flexible CLI for developing and testing Elastic's Observability projects
 				built with love by mdelapenya and friends in Go.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
