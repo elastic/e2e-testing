ca7117f249dfa398c8ebb089dc79a2db1bde23f2
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-30 21:21:15 +0100

    Support junit reporting

--- .ci/scripts/build-test.sh
+++ .ci/scripts/build-test.sh
@@ -5,4 +5,11 @@ set -euxo pipefail
 echo "Installing ${GO_VERSION} with gimme."
 eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=${GO_VERSION} bash)"
 
-make install test
+# Prepare junit build context
+mkdir -p outputs
+export OUT_FILE="outputs/test-report.out"
+
+make install test | tee ${OUT_FILE}
+
+go get -v -u github.com/t-yuki/gocover-cobertura
+go-junit-report > outputs/junit-tests.xml < ${OUT_FILE}
