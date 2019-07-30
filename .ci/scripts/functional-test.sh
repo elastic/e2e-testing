6d54e55a785a1c3b93edef3393da3db74f376a26
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-30 21:39:37 +0100

    Wrapper for the functional testing

---
+++ .ci/scripts/functional-test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Install Go using the same travis approach
+echo "Installing ${GO_VERSION} with gimme."
+eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=${GO_VERSION} bash)"
+
+# Prepare junit build context
+mkdir -p outputs
+export OUT_FILE="outputs/functional-test-report.out"
+
+make install functiona-test | tee ${OUT_FILE}
+
+go get -v -u github.com/jstemmer/go-junit-report
+go-junit-report > outputs/junit-functional-tests.xml < ${OUT_FILE}
