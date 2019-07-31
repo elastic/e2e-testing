0d1ea534a9a7643403139b77ba8f61ecc52d84cd
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-31 16:05:19 +0100

    transform to junit

--- .ci/scripts/functional-test.sh
+++ .ci/scripts/functional-test.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Install Go using the same travis approach
-echo "Installing ${GO_VERSION} with gimme."
-eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=${GO_VERSION} bash)"
+./.ci/scripts/install-go.sh
 
-# Prepare junit build context
+rm -rf outputs || true
 mkdir -p outputs
-export OUT_FILE="outputs/functional-test-report.out"
+REPORT=outputs/junit-functional-tests
 
-make functional-test | tee ${OUT_FILE}
-
-go get -v -u github.com/jstemmer/go-junit-report
-go-junit-report > outputs/junit-functional-tests.xml < ${OUT_FILE}
+make functional-test-ci | tee ${REPORT}
+sed -e 's/^[ \t]*//' ${REPORT} | grep -E '^<.*>$' > ${REPORT}.xml
--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 TEST_TIMEOUT?=5m
 
+.PHONY: install-godog
+install-godog:
+	go get github.com/DATA-DOG/godog/cmd/godog
+
 .PHONY: install
 install:
 	go get -v -t ./...
@@ -9,6 +13,9 @@ test:
 	go test -v -timeout=$(TEST_TIMEOUT) ./services
 
 .PHONY: functional-test
-functional-test:
-	go get github.com/DATA-DOG/godog/cmd/godog
+functional-test: install-godog
 	godog
+
+.PHONY: functional-test-ci
+functional-test-ci: install-godog
+	godog --format=junit
