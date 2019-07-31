bb35dd78a4ec1dd8929ccab78b8bd5324bc7791b
Manuel de la Peña <mdelapenya@gmail.com>
2019-07-31 18:58:31 +0200

    Move config initialisation to the config package, so that we can do it in tests too

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 	"os"
+	"os/user"
 	"path/filepath"
 
 	"github.com/spf13/viper"
@@ -16,6 +17,26 @@ var Op *OpConfig
 
 const fileName = "config.yml"
 
+// CheckWorkspace creates this tool workspace under user's home, in a hidden directory named ".op"
+func CheckWorkspace(services interface{}) {
+	usr, _ := user.Current()
+
+	w := filepath.Join(usr.HomeDir, ".op")
+
+	if _, err := os.Stat(w); os.IsNotExist(err) {
+		err = os.MkdirAll(w, 0755)
+		if err != nil {
+			log.Fatalf("Cannot create workdir for 'op' at "+w, err)
+		}
+
+		log.Println("'op' workdir created at " + w)
+	}
+
+	OpWorkspace = w
+
+	newConfig(w, services)
+}
+
 // OpConfig tool configuration
 type OpConfig struct {
 	Services map[string]interface{} `mapstructure:"services"`
@@ -26,8 +47,8 @@ func (c *OpConfig) GetServiceConfig(service string) interface{} {
 	return c.Services[service]
 }
 
-// NewConfig returns a new configuration
-func NewConfig(workspace string, services interface{}) {
+// newConfig returns a new configuration
+func newConfig(workspace string, services interface{}) {
 	opConfig, err := readConfig(workspace, fileName, map[string]interface{}{
 		"services": services,
 	})
