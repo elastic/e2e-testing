52dde9c15b9f44e93f6852ae24c59e05736d0bd8
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-08-01 13:38:08 +0100

    move logic to a script

---
+++ .ci/scripts/build-metricbeats.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+source ./.ci/scripts/install-go.sh
+
+go get -u -d github.com/magefile/mage
+pushd $GOPATH/src/github.com/magefile/mage
+go run bootstrap.go
+
+## For debugging purposes
+go env
+mage -version
+
+popd
+ls -ltrah
+find . -name magefile.go
+head -n 20 magefile.go || true
+mage -debug -l
+PLATFORMS=linux/amd64 mage -debug package
+docker images
