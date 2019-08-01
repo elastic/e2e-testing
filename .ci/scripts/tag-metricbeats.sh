1da13631122311a6224b511f2c2df6b3fb19070c
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-08-01 23:20:33 +0100

    as suggested in the docker import help

--- .ci/scripts/tag-metricbeats.sh
+++ .ci/scripts/tag-metricbeats.sh
@@ -2,8 +2,9 @@
 set -euxo pipefail
 
 # shellcheck disable=SC2012
-VERSION=$(ls -1 ./*docker.tar.gz | sed 's#.*metricbeat-oss-##g; s#-linux.*##g')
 DOCKER_IMAGE=$(ls -1 ./*docker.tar.gz)
-docker import < "${DOCKER_IMAGE}"
-docker tag "docker.elastic.co/beats/metricbeat-oss:${VERSION}" docker.elastic.co/beats/metricbeat-oss:PR
+
+# https://docs.docker.com/engine/reference/commandline/import/
+# shellcheck disable=SC2002
+cat "${DOCKER_IMAGE}" | docker import - docker.elastic.co/beats/metricbeat-oss:PR
 docker inspect docker.elastic.co/beats/metricbeat-oss:PR
