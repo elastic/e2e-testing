373f926458799c1640f2d11fa6132222e9d3f4b8
Manuel de la Peña <mdelapenya@gmail.com>
2019-08-01 11:34:20 +0200

    Add specific logger, using logrusorgru's Aurora

---
+++ log/logger.go
@@ -0,0 +1,33 @@
+package log
+
+import (
+	"fmt"
+	"os"
+
+	. "github.com/logrusorgru/aurora"
+)
+
+// CheckIfError should be used to naively panics if an error is not nil.
+func CheckIfError(err error) {
+	if err == nil {
+		return
+	}
+
+	Error(err.Error())
+	os.Exit(1)
+}
+
+// Error should be used to describe error messages.
+func Error(format string, args ...interface{}) {
+	fmt.Printf("%s\n", Bold(Red(fmt.Sprintf(format, args...))))
+}
+
+// Info should be used to describe the example commands that are about to run.
+func Info(format string, args ...interface{}) {
+	fmt.Printf("%s\n", Bold(Blue(fmt.Sprintf(format, args...))))
+}
+
+// Success should be used to describe success messages.
+func Success(format string, args ...interface{}) {
+	fmt.Printf("%s\n", Bold(Green(fmt.Sprintf(format, args...))))
+}
