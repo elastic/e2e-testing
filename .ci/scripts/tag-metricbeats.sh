c9b4e5b176f0808a5e87229603dac10b338decfa
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-08-01 22:21:33 +0100

    Different approach to run within another worker

--- .ci/scripts/tag-metricbeats.sh
+++ .ci/scripts/tag-metricbeats.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-LOCATION=${1}
-
-pushd "${LOCATION}"/build/distributions
-
 # shellcheck disable=SC2012
 VERSION=$(ls -1 ./*docker.tar.gz | sed 's#.*metricbeat-oss-##g; s#-linux.*##g')
-
+DOCKER_IMAGE=$(ls -1 ./*docker.tar.gz)
+docker import < "${DOCKER_IMAGE}"
 docker tag "docker.elastic.co/beats/metricbeat-oss:${VERSION}" docker.elastic.co/beats/metricbeat-oss:PR
 docker inspect docker.elastic.co/beats/metricbeat-oss:PR
