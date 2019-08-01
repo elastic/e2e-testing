79387317b2a78122e4bd31b781841a4a372dea72
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-08-01 16:33:10 +0100

    fix the replace

--- .ci/scripts/tag-metricbeats.sh
+++ .ci/scripts/tag-metricbeats.sh
@@ -6,7 +6,7 @@ LOCATION=${1}
 pushd "${LOCATION}"/build/distributions
 
 # shellcheck disable=SC2012
-VERSION=$(ls -1 ./*docker.tar.gz | sed 's#metricbeat-oss-##g; s#-linux.*##g')
+VERSION=$(ls -1 ./*docker.tar.gz | sed 's#.*metricbeat-oss-##g; s#-linux.*##g')
 
 docker tag "docker.elastic.co/beats/metricbeat-oss:${VERSION}" docker.elastic.co/beats/metricbeat-oss:PR
 docker inspect docker.elastic.co/beats/metricbeat-oss:PR
