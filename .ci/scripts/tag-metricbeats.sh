87febb40386edc1412e62b6e022d49443e89936f
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-08-02 10:27:33 +0100

    Let's ensure the version within the beats repo is also used when importing the docker image

--- .ci/scripts/tag-metricbeats.sh
+++ .ci/scripts/tag-metricbeats.sh
@@ -3,5 +3,9 @@ set -euxo pipefail
 
 # shellcheck disable=SC2012
 DOCKER_IMAGE=$(ls -1 ./*docker.tar.gz)
+VERSION=$(echo "${DOCKER_IMAGE}" | sed 's#.*metricbeat-oss-##g; s#-linux.*##g')
 
-docker import "${DOCKER_IMAGE}"
+# https://docs.docker.com/engine/reference/commandline/import/
+# shellcheck disable=SC2002
+cat "${DOCKER_IMAGE}" | docker import - docker.elastic.co/beats/metricbeat-oss:"${VERSION}"
+docker inspect docker.elastic.co/beats/metricbeat-oss:"${VERSION}"
