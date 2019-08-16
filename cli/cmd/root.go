42f167f2c467e2f6f734ad4ea53f198d3381bb63
Manuel de la Peña <mdelapenya@gmail.com>
2019-08-16 08:35:00 +0200

    SF: add emoji to default command message

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -13,7 +13,7 @@ var rootCmd = &cobra.Command{
 	Use:   "op",
 	Short: "op (Observability Provisioner) makes it easier to develop Observability projects.",
 	Long: `A Fast and Flexible CLI for developing and testing Elastic's Observability projects
-				built with love by mdelapenya and friends in Go.`,
+	built with ❤️ by mdelapenya and friends in Go.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
 	},
