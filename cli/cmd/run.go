578b7de5fee2542de49e16955173172a3b249df7
Manuel de la Peña <mdelapenya@gmail.com>
2019-08-27 15:30:31 +0200

    Wait for Kibana's status endpoint instead of a log entry

--- cli/cmd/run.go
+++ cli/cmd/run.go
@@ -92,7 +92,7 @@ func buildRunStackCommand(key string, stack config.Stack) *cobra.Command {
 				s := serviceManager.BuildFromConfig(srv)
 
 				if k == "kibana" {
-					s.SetWaitFor(wait.ForLog("http server running"))
+					s.SetWaitFor(wait.ForHTTP("/api/status"))
 				}
 
 				if k == "elasticsearch" {
