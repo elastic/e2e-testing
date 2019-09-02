039eb2e8db16fe4198b11e825d9ed9d06baa019e
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-02 12:48:09 +0100

    Disable GOPROXY for the install dependencies for the preCommit

--- .ci/scripts/install-dependencies.sh
+++ .ci/scripts/install-dependencies.sh
@@ -12,6 +12,10 @@ GO_VERSION=${1:?GO_VERSION is not set}
 # shellcheck disable=SC1091
 source .ci/scripts/install-go.sh "${GO_VERSION}"
 
+# disable GOPROXY temporarily
+GOPROXY_CONTEXT=${GOPROXY}
+GOPROXY=''
+
 # Install some other dependencies required for the pre-commit
 go get -v -u golang.org/x/lint/golint
 go get -v -u github.com/golangci/golangci-lint/cmd/golangci-lint
@@ -20,6 +24,9 @@ go get -v -u github.com/go-critic/go-critic/...
 
 # Gometalinter is deprecated so let's use the sh instead
 curl https://raw.githubusercontent.com/alecthomas/gometalinter/master/scripts/install.sh | sh
+
+# enable GOPROXY
+GOPROXY=${GOPROXY_CONTEXT}
 
 # Install project dependencies
 make -C cli install
