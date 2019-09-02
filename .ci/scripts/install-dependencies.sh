c63fd662d4145ac40e2bf8a431081679bb76b2e8
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-02 12:20:04 +0100

    It's required to use GO MODULE for the execution of the make install

--- .ci/scripts/install-dependencies.sh
+++ .ci/scripts/install-dependencies.sh
@@ -14,10 +14,12 @@ source .ci/scripts/install-go.sh "${GO_VERSION}"
 
 # Install some other dependencies required for the pre-commit
 go get -v -u golang.org/x/lint/golint
-go get -v -u github.com/alecthomas/gometalinter
 go get -v -u github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -v -u github.com/go-lintpack/lintpack/...
 go get -v -u github.com/go-critic/go-critic/...
+
+# Gometalinter is deprecated so let's use the sh instead
+curl https://raw.githubusercontent.com/alecthomas/gometalinter/master/scripts/install.sh | sh
 
 # Install project dependencies
 make -C cli install
