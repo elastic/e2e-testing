5aacffc91290c6524cc1d8c31eb2e279ce1d43bb
Victor Martinez <victormartinezrubio@gmail.com>
2019-09-02 13:06:11 +0100

    fix: gometalinter got issues with go modules (#25)

--- .ci/scripts/install-dependencies.sh
+++ .ci/scripts/install-dependencies.sh
@@ -12,12 +12,21 @@ GO_VERSION=${1:?GO_VERSION is not set}
 # shellcheck disable=SC1091
 source .ci/scripts/install-go.sh "${GO_VERSION}"
 
+# disable GOPROXY temporarily
+GOPROXY_CONTEXT=${GOPROXY}
+GOPROXY=''
+
 # Install some other dependencies required for the pre-commit
 go get -v -u golang.org/x/lint/golint
-go get -v -u github.com/alecthomas/gometalinter
 go get -v -u github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -v -u github.com/go-lintpack/lintpack/...
 go get -v -u github.com/go-critic/go-critic/...
+
+# Gometalinter is deprecated so let's use the sh instead
+curl https://raw.githubusercontent.com/alecthomas/gometalinter/master/scripts/install.sh | sh
+
+# enable GOPROXY
+GOPROXY=${GOPROXY_CONTEXT}
 
 # Install project dependencies
 make -C cli install
