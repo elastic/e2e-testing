a70f48b4bbb6f136ccaf6c2aad947da14d653af0
Manuel de la Peña <mdelapenya@gmail.com>
2019-09-24 09:07:22 +0200

    (#42) SF: reorder methods alphabetically

--- cli/config/config.go
+++ cli/config/config.go
@@ -20,6 +20,13 @@ var opComposeBox *packr.Box
 // Op the tool's configuration, read from tool's workspace
 var Op *OpConfig
 
+// OpConfig tool configuration
+type OpConfig struct {
+	Services  map[string]Service `mapstructure:"services"`
+	Stacks    map[string]Stack   `mapstructure:"stacks"`
+	Workspace string             `mapstructure:"workspace"`
+}
+
 // Service represents the configuration for a service
 type Service struct {
 	Name string `mapstructure:"Name"`
@@ -33,48 +40,6 @@ type Stack struct {
 	Path string `mapstructure:"Path"`
 }
 
-// checkInstalledSoftware checks that the required software is present
-func checkInstalledSoftware() {
-	log.Debug("Validating required tools...")
-	binaries := []string{
-		"docker",
-		"docker-compose",
-	}
-
-	for _, binary := range binaries {
-		which(binary)
-	}
-}
-
-// Init creates this tool workspace under user's home, in a hidden directory named ".op"
-func Init() {
-	configureLogger()
-
-	checkInstalledSoftware()
-
-	InitConfig()
-}
-
-// InitConfig initialises configuration
-func InitConfig() {
-	if Op != nil {
-		return
-	}
-
-	usr, _ := user.Current()
-
-	w := filepath.Join(usr.HomeDir, ".op")
-
-	newConfig(w)
-}
-
-// OpConfig tool configuration
-type OpConfig struct {
-	Services  map[string]Service `mapstructure:"services"`
-	Stacks    map[string]Stack   `mapstructure:"stacks"`
-	Workspace string             `mapstructure:"workspace"`
-}
-
 // AvailableServices return the services in the configuration file
 func AvailableServices() map[string]Service {
 	return Op.Services
@@ -83,155 +48,6 @@ func AvailableServices() map[string]Service {
 // AvailableStacks return the stacks in the configuration file
 func AvailableStacks() map[string]Stack {
 	return Op.Stacks
-}
-
-// GetServiceConfig configuration of a service
-func GetServiceConfig(service string) (Service, bool) {
-	return Op.GetServiceConfig(service)
-}
-
-// GetServiceConfig configuration of a service
-func (c *OpConfig) GetServiceConfig(service string) (Service, bool) {
-	srv, exists := c.Services[service]
-
-	return srv, exists
-}
-
-func checkConfigDirs(workspace string) {
-	servicesPath := path.Join(workspace, "compose", "services")
-	stacksPath := path.Join(workspace, "compose", "stacks")
-
-	checkConfigDirectory(servicesPath)
-	checkConfigDirectory(stacksPath)
-
-	log.WithFields(log.Fields{
-		"servicesPath": servicesPath,
-		"stacksPath":   stacksPath,
-	}).Debug("'op' workdirs created.")
-}
-
-func checkConfigDirectory(dir string) {
-	found, err := exists(dir)
-	if found && err == nil {
-		return
-	}
-	err = os.MkdirAll(dir, 0755)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-			"path":  dir,
-		}).Fatal("Cannot create directory")
-	}
-}
-
-// newConfig returns a new configuration
-func newConfig(workspace string) {
-	if Op != nil {
-		return
-	}
-
-	checkConfigDirs(workspace)
-
-	opConfig := OpConfig{
-		Services:  map[string]Service{},
-		Stacks:    map[string]Stack{},
-		Workspace: workspace,
-	}
-	Op = &opConfig
-
-	box := packComposeFiles(Op)
-	if box != nil {
-		log.WithFields(log.Fields{
-			"workspace": workspace,
-		}).Error("Could not get packaged compose files")
-		return
-	}
-
-	opComposeBox = box
-}
-
-func configureLogger() {
-	includeTimestamp := os.Getenv("OP_LOG_INCLUDE_TIMESTAMP")
-	fullTimestamp := (strings.ToUpper(includeTimestamp) == "TRUE")
-
-	log.SetFormatter(&log.TextFormatter{
-		FullTimestamp: fullTimestamp,
-	})
-
-	switch logLevel := os.Getenv("OP_LOG_LEVEL"); logLevel {
-	case "TRACE":
-		log.SetLevel(log.TraceLevel)
-	case "DEBUG":
-		log.SetLevel(log.DebugLevel)
-	case "WARNING":
-		log.SetLevel(log.WarnLevel)
-	case "ERROR":
-		log.SetLevel(log.ErrorLevel)
-	case "FATAL":
-		log.SetLevel(log.FatalLevel)
-	case "PANIC":
-		log.SetLevel(log.PanicLevel)
-	default:
-		log.SetLevel(log.InfoLevel)
-	}
-}
-
-func exists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return true, err
-}
-
-// which checks if software is installed
-func which(software string) {
-	path, err := exec.LookPath(software)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error":    err,
-			"software": software,
-		}).Fatal("Required binary is not present")
-	}
-
-	log.WithFields(log.Fields{
-		"software": software,
-		"path":     path,
-	}).Debug("Binary is present")
-}
-
-func packComposeFiles(op *OpConfig) *packr.Box {
-	box := packr.New("Compose Files", "./compose")
-
-	err := box.Walk(func(boxedPath string, f packr.File) error {
-		log.WithFields(log.Fields{
-			"path": boxedPath,
-		}).Debug("Boxed file")
-
-		composeType, composeNameWithExtension := path.Split(boxedPath)
-		composeName := strings.TrimSuffix(composeNameWithExtension, filepath.Ext(composeNameWithExtension))
-		if composeType == "stacks/" {
-			op.Stacks[composeName] = Stack{
-				Name: composeName,
-				Path: boxedPath,
-			}
-		} else if composeType == "services/" {
-			op.Services[composeName] = Service{
-				Name: composeName,
-				Path: boxedPath,
-			}
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil
-	}
-
-	return box
 }
 
 // GetPackedCompose returns the path of the compose file, looking up the
@@ -290,4 +106,188 @@ func GetPackedCompose(isStack bool, composeName string) (string, error) {
 	}).Debug("Compose file generated at workdir.")
 
 	return composeFilePath, nil
+}
+
+// GetServiceConfig configuration of a service
+func GetServiceConfig(service string) (Service, bool) {
+	return Op.GetServiceConfig(service)
+}
+
+// GetServiceConfig configuration of a service
+func (c *OpConfig) GetServiceConfig(service string) (Service, bool) {
+	srv, exists := c.Services[service]
+
+	return srv, exists
+}
+
+// Init creates this tool workspace under user's home, in a hidden directory named ".op"
+func Init() {
+	configureLogger()
+
+	checkInstalledSoftware()
+
+	InitConfig()
+}
+
+// InitConfig initialises configuration
+func InitConfig() {
+	if Op != nil {
+		return
+	}
+
+	usr, _ := user.Current()
+
+	w := filepath.Join(usr.HomeDir, ".op")
+
+	newConfig(w)
+}
+
+func checkConfigDirectory(dir string) {
+	found, err := exists(dir)
+	if found && err == nil {
+		return
+	}
+	err = os.MkdirAll(dir, 0755)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+			"path":  dir,
+		}).Fatal("Cannot create directory")
+	}
+}
+
+func checkConfigDirs(workspace string) {
+	servicesPath := path.Join(workspace, "compose", "services")
+	stacksPath := path.Join(workspace, "compose", "stacks")
+
+	checkConfigDirectory(servicesPath)
+	checkConfigDirectory(stacksPath)
+
+	log.WithFields(log.Fields{
+		"servicesPath": servicesPath,
+		"stacksPath":   stacksPath,
+	}).Debug("'op' workdirs created.")
+}
+
+// checkInstalledSoftware checks that the required software is present
+func checkInstalledSoftware() {
+	log.Debug("Validating required tools...")
+	binaries := []string{
+		"docker",
+		"docker-compose",
+	}
+
+	for _, binary := range binaries {
+		which(binary)
+	}
+}
+
+func configureLogger() {
+	includeTimestamp := os.Getenv("OP_LOG_INCLUDE_TIMESTAMP")
+	fullTimestamp := (strings.ToUpper(includeTimestamp) == "TRUE")
+
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: fullTimestamp,
+	})
+
+	switch logLevel := os.Getenv("OP_LOG_LEVEL"); logLevel {
+	case "TRACE":
+		log.SetLevel(log.TraceLevel)
+	case "DEBUG":
+		log.SetLevel(log.DebugLevel)
+	case "WARNING":
+		log.SetLevel(log.WarnLevel)
+	case "ERROR":
+		log.SetLevel(log.ErrorLevel)
+	case "FATAL":
+		log.SetLevel(log.FatalLevel)
+	case "PANIC":
+		log.SetLevel(log.PanicLevel)
+	default:
+		log.SetLevel(log.InfoLevel)
+	}
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+// newConfig returns a new configuration
+func newConfig(workspace string) {
+	if Op != nil {
+		return
+	}
+
+	checkConfigDirs(workspace)
+
+	opConfig := OpConfig{
+		Services:  map[string]Service{},
+		Stacks:    map[string]Stack{},
+		Workspace: workspace,
+	}
+	Op = &opConfig
+
+	box := packComposeFiles(Op)
+	if box != nil {
+		log.WithFields(log.Fields{
+			"workspace": workspace,
+		}).Error("Could not get packaged compose files")
+		return
+	}
+
+	opComposeBox = box
+}
+
+func packComposeFiles(op *OpConfig) *packr.Box {
+	box := packr.New("Compose Files", "./compose")
+
+	err := box.Walk(func(boxedPath string, f packr.File) error {
+		log.WithFields(log.Fields{
+			"path": boxedPath,
+		}).Debug("Boxed file")
+
+		composeType, composeNameWithExtension := path.Split(boxedPath)
+		composeName := strings.TrimSuffix(composeNameWithExtension, filepath.Ext(composeNameWithExtension))
+		if composeType == "stacks/" {
+			op.Stacks[composeName] = Stack{
+				Name: composeName,
+				Path: boxedPath,
+			}
+		} else if composeType == "services/" {
+			op.Services[composeName] = Service{
+				Name: composeName,
+				Path: boxedPath,
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil
+	}
+
+	return box
+}
+
+// which checks if software is installed
+func which(software string) {
+	path, err := exec.LookPath(software)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":    err,
+			"software": software,
+		}).Fatal("Required binary is not present")
+	}
+
+	log.WithFields(log.Fields{
+		"software": software,
+		"path":     path,
+	}).Debug("Binary is present")
 }
