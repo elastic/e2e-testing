27f6a4a3b446f45b715cdc13a35f242be0222bab
Manuel de la Peña <mdelapenya@gmail.com>
2019-09-24 14:43:40 +0200

    (#42) Fix metricbeat initialisation

--- metricbeat-tests/metricbeat.go
+++ metricbeat-tests/metricbeat.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path"
 
 	log "github.com/sirupsen/logrus"
 
@@ -10,26 +12,21 @@ import (
 
 // RunMetricbeatService runs a metricbeat service entity for a service to monitor
 func RunMetricbeatService(version string, serviceName string, serviceVersion string) error {
-	// TO-DO: we have to see how to mount the configuration file into the compose
-	/*
-		dir, _ := os.Getwd()
-		bindMounts := map[string]string{
-			dir + "/configurations/" + serviceName + ".yml": "/usr/share/metricbeat/metricbeat.yml",
-		}
-	*/
+	dir, _ := os.Getwd()
 	indexName := fmt.Sprintf("metricbeat-%s-%s-%s-test", version, serviceName, serviceVersion)
 
 	serviceManager := services.NewServiceManager()
 
 	env := map[string]string{
-		"BEAT_STRICT_PERMS": "false",
-		"indexName":         indexName,
-		"metricbeatTag":     version,
-		serviceName + "Tag": serviceVersion,
-		"serviceName":       serviceName,
+		"BEAT_STRICT_PERMS":    "false",
+		"indexName":            indexName,
+		"metricbeatConfigFile": path.Join(dir, "configurations", serviceName+".yml"),
+		"metricbeatTag":        version,
+		serviceName + "Tag":    serviceVersion,
+		"serviceName":          serviceName,
 	}
 
-	err := serviceManager.AddServicesToCompose("metricbeat", []string{serviceName}, env)
+	err := serviceManager.AddServicesToCompose("metricbeat", []string{"metricbeat"}, env)
 	if err != nil {
 		msg := fmt.Sprintf("Could not run Metricbeat %s for %s %v", version, serviceName, err)
 
