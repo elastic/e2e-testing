5539dd705f3f637bf3f182ae1629adc619253c2e
Manuel de la Peña <mdelapenya@gmail.com>
2019-09-24 13:32:50 +0200

    (#42) Add a command to undeploy a service from a stack

--- cli/cmd/deploy.go
+++ cli/cmd/deploy.go
@@ -14,21 +14,38 @@ func init() {
 	config.InitConfig()
 
 	for k := range config.AvailableServices() {
-		serviceSubcommand := buildDeployServiceCommand(k)
+		// deploy command
+		deployServiceSubcommand := buildDeployServiceCommand(k)
 
-		serviceSubcommand.Flags().StringVarP(&deployToStack, "stack", "s", "", "Sets the stack where to deploy the service. (Required)")
-		serviceSubcommand.Flags().StringVarP(&versionToRun, "version", "v", "latest", "Sets the image version to run")
+		deployServiceSubcommand.Flags().StringVarP(&deployToStack, "stack", "s", "", "Sets the stack where to deploy the service. (Required)")
+		deployServiceSubcommand.Flags().StringVarP(&versionToRun, "version", "v", "latest", "Sets the image version to run")
 
-		deployCmd.AddCommand(serviceSubcommand)
+		deployCmd.AddCommand(deployServiceSubcommand)
+
+		// undeploy command
+		undeployServiceSubcommand := buildUndeployServiceCommand(k)
+		undeployServiceSubcommand.Flags().StringVarP(&deployToStack, "stack", "s", "", "Sets the stack where to undeploy the service. (Required)")
+
+		undeployCmd.AddCommand(undeployServiceSubcommand)
 	}
 
 	rootCmd.AddCommand(deployCmd)
+	rootCmd.AddCommand(undeployCmd)
 }
 
 var deployCmd = &cobra.Command{
 	Use:   "deploy",
 	Short: "Deploys a Service to a Stack",
 	Long:  "Deploys a Service to a Stack",
+	Run: func(cmd *cobra.Command, args []string) {
+		// NOOP
+	},
+}
+
+var undeployCmd = &cobra.Command{
+	Use:   "undeploy",
+	Short: "Undeploys a Service from a Stack",
+	Long:  "Undeploys a Service from a Stack",
 	Run: func(cmd *cobra.Command, args []string) {
 		// NOOP
 	},
@@ -52,6 +69,25 @@ func buildDeployServiceCommand(srv string) *cobra.Command {
 					"stack":    deployToStack,
 					"services": servicesToRun,
 				}).Error("Could not add services to the stack.")
+			}
+		},
+	}
+}
+
+func buildUndeployServiceCommand(srv string) *cobra.Command {
+	return &cobra.Command{
+		Use:   srv,
+		Short: `Undeploys a ` + srv + ` service`,
+		Long:  `Undeploys a ` + srv + ` service, removing it from a running stack, identified by its name`,
+		Run: func(cmd *cobra.Command, args []string) {
+			serviceManager := services.NewServiceManager()
+
+			err := serviceManager.RemoveServicesFromCompose(deployToStack, []string{srv})
+			if err != nil {
+				log.WithFields(log.Fields{
+					"stack":    deployToStack,
+					"services": servicesToRun,
+				}).Error("Could not remove services from the stack.")
 			}
 		},
 	}
