d1990f55c0c7f78e5a95334a8d91badc5a6ee924
Manuel de la Peña <mdelapenya@gmail.com>
2019-10-28 14:13:22 +0100

    Use same env vars as in the CLI side for Gimme

--- .ci/functionalTests.groovy
+++ .ci/functionalTests.groovy
@@ -99,8 +99,9 @@ pipeline {
       options { skipDefaultCheckout() }
       environment {
         GO111MODULE = 'on'
+        GOROOT = "${env.WORKSPACE}/.gimme/versions/go${env.GO_VERSION}.linux.amd64"
+        PATH = "${env.WORKSPACE}/${env.BASE_DIR}/bin:${env.GOROOT}/bin:${env.GOPATH}/bin:${env.PATH}"
         GOPROXY = 'https://proxy.golang.org'
-        PATH = "${env.GOPATH}/bin:${env.PATH}"
         STACK_VERSION = "${params.STACK_VERSION}"
         METRICBEAT_VERSION = "${params.METRICBEAT_VERSION}"
       }
