29273f30433eb5e571b5fa030a16e911783c2a33
Manuel de la Peña <mdelapenya@gmail.com>
2019-10-30 18:05:28 +0100

    (#69) Do not use a docker container for packr2 process
    Let's install it in the CI machine

--- cli/build.sh
+++ cli/build.sh
@@ -4,40 +4,46 @@ set -exuo pipefail
 readonly supportedOSS=("darwin" "linux" "windows")
 readonly supportedArchs=("386" "amd64")
 
-# GOOS represents the Golang's supported Operative Systems.
+# GO_OS represents the Golang's supported Operative Systems.
 # Possible values: darwin, linux, windows
-readonly GOOS="${GOOS:-linux}"
+readonly GO_OS="${GOOS:-linux}"
 if [[ ! " ${supportedOSS[@]} " =~ " ${GOOS} " ]]; then
-    echo "It's not possible to build a binary for ${GOOS}. Supported values: darwin, linux, windows"
+    echo "It's not possible to build a binary for ${GO_OS}. Supported values: darwin, linux, windows"
     exit 1
 fi
 
-# GOARCH represents the Golang's supported Architectures.
+# GO_ARCH represents the Golang's supported Architectures.
 # Possible values: 386, amd64
-readonly GOARCH="${GOARCH:-amd64}"
-if [[ ! " ${supportedArchs[@]} " =~ " ${GOARCH} " ]]; then
-    echo "It's not possible to build a binary for ${GOARCH}. Supported values: 386, amd64"
+readonly GO_ARCH="${GOARCH:-amd64}"
+if [[ ! " ${supportedArchs[@]} " =~ " ${GO_ARCH} " ]]; then
+    echo "It's not possible to build a binary for ${GO_ARCH}. Supported values: 386, amd64"
     exit 1
 fi
 
-readonly GO_VERSION="${GO_VERSION:-v1.12.7}"
-readonly GO_WORKSPACE="/usr/local/go/src/github.com/elastic/op"
+readonly GO_VERSION="${GO_VERSION:-1.13.0}"
 readonly VERSION="$(cat ./VERSION.txt)"
 
-arch="${GOARCH}"
-goos="${GOOS}"
+arch="${GO_ARCH}"
+goos="${GO_OS}"
 extension=""
 
-if [[ "$GOARCH" == "amd64" ]]; then
+if [[ "$GO_ARCH" == "amd64" ]]; then
     arch="64"
 fi
 
-if [[ "$GOOS" == "windows" ]]; then
+if [[ "$GO_OS" == "windows" ]]; then
     goos="win"
     extension=".exe"
 fi
 
-echo ">>> Building for ${GOOS}/${GOARCH}"
-docker run --rm -v "$(pwd)":${GO_WORKSPACE} -w ${GO_WORKSPACE} \
-    -e GOOS=${GOOS} -e GOARCH=${GOARCH} registry.hub.docker.com/drud/golang-build-container:v${GO_VERSION} \
-    packr2 build -v -o ${GO_WORKSPACE}/.github/releases/download/${VERSION}/${goos}${arch}-op${extension}
+echo ">>> Installing Packr2"
+go get -u github.com/gobuffalo/packr/v2/packr2@v2.7.1
+
+echo ">>> Generating Packr boxes"
+packr2
+
+echo ">>> Building for ${GO_OS}/${GO_ARCH}"
+GOOS=${GO_OS} GOARCH=${GO_ARCH} go build -v -o $(pwd)/.github/releases/download/${VERSION}/${goos}${arch}-op${extension}
+
+echo ">>> Cleaning up Packr boxes"
+packr2 clean
