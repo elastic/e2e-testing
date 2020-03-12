56022adc3c2a80f2f84471900897353662bc2268
Manuel de la Peña <mdelapenya@gmail.com>
2020-03-12 12:34:33 +0100

    chore: install test dependencies in the build agents

---
+++ .ci/scripts/install-test-dependencies.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+#
+# Install the dependencies using in tests.
+#
+
+# install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+
+# install helm
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
