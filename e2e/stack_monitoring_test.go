3b626550ca7722835327cc799b1b820c294f5783
Manuel de la Peña <mdelapenya@gmail.com>
2020-04-24 23:44:38 +0200

    chore: start/destroy runtime dependencies for stack monitoring tests
    It needs an elasticsearch as monitoring instance, and we will create them
    using the life cycle hooks (Befores and Afters)

--- e2e/stack_monitoring_test.go
+++ e2e/stack_monitoring_test.go
@@ -1,8 +1,11 @@
 package e2e
 
 import (
+	"time"
+
 	"github.com/cucumber/godog"
 	messages "github.com/cucumber/messages-go/v10"
+	"github.com/elastic/metricbeat-tests-poc/cli/services"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -64,7 +67,28 @@ func StackMonitoringFeatureContext(s *godog.Suite) {
 
 	s.BeforeSuite(func() {
 		log.Debug("Before StackMonitoring Suite...")
+
+		env := map[string]string{
+			"stackVersion":              stackVersion,
+			"xpackMonitoringCollection": "false",
+		}
+
 		log.Debug("Installing elasticsearch monitoring instance")
+		err := serviceManager.RunCompose(true, []string{"stack-monitoring"}, env)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"stack": "stack-monitoring",
+			}).Error("Could not run the stack.")
+		}
+
+		minutesToBeHealthy := 3 * time.Minute
+		healthy, err := waitForElasticsearch(minutesToBeHealthy, "metricbeat")
+		if !healthy {
+			log.WithFields(log.Fields{
+				"error":   err,
+				"minutes": minutesToBeHealthy,
+			}).Error("The Elasticsearch cluster could not get the healthy status")
+		}
 	})
 	s.BeforeScenario(func(*messages.Pickle) {
 		log.Debug("Before StackMonitoring Scenario...")
@@ -72,6 +96,14 @@ func StackMonitoringFeatureContext(s *godog.Suite) {
 	s.AfterSuite(func() {
 		log.Debug("After StackMonitoring Suite...")
 		log.Debug("Destroying elasticsearch monitoring instance, including attached services")
+		serviceManager := services.NewServiceManager()
+
+		err := serviceManager.StopCompose(true, []string{"stack-monitoring"})
+		if err != nil {
+			log.WithFields(log.Fields{
+				"stack": "stack-monitoring",
+			}).Error("Could not stop the stack.")
+		}
 	})
 	s.AfterScenario(func(*messages.Pickle, error) {
 		log.Debug("After StackMonitoring Scenario...")
