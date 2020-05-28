d91f6a90a8b94d8c24cb1338f05357b24ebd8049
Manuel de la Peña <mdelapenya@gmail.com>
2020-05-28 23:05:48 +0200

    chore: update automation for the new layout

--- .ci/scripts/functional-test.sh
+++ .ci/scripts/functional-test.sh
@@ -10,9 +10,10 @@ set -euxo pipefail
 #
 
 GO_VERSION=${1:?GO_VERSION is not set}
-FEATURE=${2:-''}
-STACK_VERSION=${3:-'7.6.0'}
-METRICBEAT_VERSION=${4:-'7.6.0'}
+SUITE=${2:-''}
+FEATURE=${3:-''}
+STACK_VERSION=${4:-'7.6.0'}
+METRICBEAT_VERSION=${5:-'7.6.0'}
 TARGET_OS=${GOOS:-linux}
 TARGET_ARCH=${GOARCH:-amd64}
 
@@ -31,7 +32,7 @@ mkdir -p outputs
 ## Parse FEATURE if not ALL then enable the flags to be passed to the functional-test wrapper
 REPORT=''
 if [ "${FEATURE}" != "" ] && [ "${FEATURE}" != "all" ] ; then
-  REPORT=outputs/TEST-${FEATURE}
+  REPORT=outputs/TEST-${SUITE}-${FEATURE}
 else
   FEATURE=''
   REPORT=outputs/TEST-functional-tests
@@ -40,12 +41,11 @@ fi
 ## Generate test report even if make failed.
 set +e
 exit_status=0
-if ! FEATURE=${FEATURE} FORMAT=junit STACK_VERSION=${STACK_VERSION} METRICBEAT_VERSION=${METRICBEAT_VERSION} make --no-print-directory -C e2e functional-test | tee ${REPORT}  ; then
+if ! SUITE=${SUITE} FEATURE=${FEATURE} FORMAT=junit STACK_VERSION=${STACK_VERSION} METRICBEAT_VERSION=${METRICBEAT_VERSION} make --no-print-directory -C e2e functional-test | tee ${REPORT}  ; then
   echo 'ERROR: functional-test failed'
   exit_status=1
 fi
 
 ## Transform report to Junit by parsing the stdout generated previously
-sed -i "s/testing: warning: no tests to run//" ${REPORT}
 sed -e 's/^[ \t]*//; s#>.*failed$#>#g' ${REPORT} | grep -E '^<.*>$' > ${REPORT}.xml
 exit $exit_status
