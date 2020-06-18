3ae55868a784958d09c48baf69327c2df4a74966
Manuel de la Peña <mdelapenya@gmail.com>
2020-06-18 23:37:13 +0200

    feat: check that there are hits in the index

--- e2e/_suites/ingest-manager/stand-alone.go
+++ e2e/_suites/ingest-manager/stand-alone.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
+	"time"
+
 	"github.com/cucumber/godog"
+	"github.com/elastic/e2e-testing/cli/docker"
 	"github.com/elastic/e2e-testing/cli/services"
 	"github.com/elastic/e2e-testing/e2e"
 	log "github.com/sirupsen/logrus"
@@ -66,7 +70,110 @@ func (sats *StandAloneTestSuite) aStandaloneAgentIsDeployed() error {
 }
 
 func (sats *StandAloneTestSuite) thereIsNewDataInTheIndexFromAgent() error {
-	return godog.ErrPending
+	timezone := "America/New_York"
+	now := time.Now()
+	startDate := now.Add(-15 * time.Minute)
+
+	serviceName := "ingest-manager_elastic-agent_1"
+	hostname, err := getContainerName(serviceName)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":   err,
+			"service": serviceName,
+		}).Error("Could not retrieve container name from the Docker client")
+		return err
+	}
+
+	esQuery := map[string]interface{}{
+		"version": true,
+		"size":    500,
+		"docvalue_fields": []map[string]interface{}{
+			{
+				"field":  "@timestamp",
+				"format": "date_time",
+			},
+			{
+				"field":  "system.process.cpu.start_time",
+				"format": "date_time",
+			},
+			{
+				"field":  "system.service.state_since",
+				"format": "date_time",
+			},
+		},
+		"_source": map[string]interface{}{
+			"excludes": []map[string]interface{}{},
+		},
+		"query": map[string]interface{}{
+			"bool": map[string]interface{}{
+				"must": []map[string]interface{}{},
+				"filter": []map[string]interface{}{
+					{
+						"bool": map[string]interface{}{
+							"filter": []map[string]interface{}{
+								{
+									"bool": map[string]interface{}{
+										"should": []map[string]interface{}{
+											{
+												"match_phrase": map[string]interface{}{
+													"host.name": hostname,
+												},
+											},
+										},
+										"minimum_should_match": 1,
+									},
+								},
+								{
+									"bool": map[string]interface{}{
+										"should": []map[string]interface{}{
+											{
+												"range": map[string]interface{}{
+													"@timestamp": map[string]interface{}{
+														"gte":       now,
+														"time_zone": timezone,
+													},
+												},
+											},
+										},
+										"minimum_should_match": 1,
+									},
+								},
+							},
+						},
+					},
+					{
+						"range": map[string]interface{}{
+							"@timestamp": map[string]interface{}{
+								"gte":    startDate,
+								"format": "strict_date_optional_time",
+							},
+						},
+					},
+				},
+				"should":   []map[string]interface{}{},
+				"must_not": []map[string]interface{}{},
+			},
+		},
+	}
+
+	indexName := "logs-agent-default"
+
+	result, err := e2e.RetrySearch(indexName, esQuery, queryMaxAttempts, queryRetryTimeout)
+	if err != nil {
+		return err
+	}
+
+	log.Debugf("Search result: %v", result)
+
+	err = e2e.AssertHitsArePresent(result)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"index": indexName,
+		}).Error(err.Error())
+		return err
+	}
+
+	return nil
 }
 
 func (sats *StandAloneTestSuite) theDockerContainerIsStopped(arg1 string) error {
@@ -75,4 +182,22 @@ func (sats *StandAloneTestSuite) theDockerContainerIsStopped(arg1 string) error 
 
 func (sats *StandAloneTestSuite) thereIsNoNewDataInTheIndexAfterAgentShutsDown() error {
 	return godog.ErrPending
+}
+
+func getContainerName(serviceName string) (string, error) {
+	log.WithFields(log.Fields{
+		"service": serviceName,
+	}).Debug("Retrieving container name from the Docker client")
+
+	containerName, err := docker.ExecCommandIntoContainer(context.Background(), serviceName, "root", []string{"hostname"})
+	if err != nil {
+		return "", err
+	}
+
+	log.WithFields(log.Fields{
+		"containerName": containerName,
+		"service":       serviceName,
+	}).Debug("Container name retrieved from the Docker client")
+
+	return containerName, nil
 }
