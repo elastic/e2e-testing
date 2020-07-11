13a42a8bb298d494fd71ea42e20594d23b43e5fb
Manuel de la Peña <mdelapenya@gmail.com>
2020-07-11 17:36:40 +0200

    chore: add Elastic's x-pack license to all Go files ans shell scripts

--- .ci/scripts/install-test-dependencies.sh
+++ .ci/scripts/install-test-dependencies.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+## Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+## or more contributor license agreements. Licensed under the Elastic License;
+## you may not use this file except in compliance with the Elastic License.
+
 set -euxo pipefail
 #
 # Install the dependencies using the install and test make goals.
