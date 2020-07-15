bf7ca4cbc36a1adc3b5ee4f5e0cfe83182ec6093
Manuel de la Peña <mdelapenya@gmail.com>
2020-07-15 10:30:48 +0200

    chore: simplify scheduled pipelines
    We prefer using the downstream one for custom executions

--- .ci/e2eTestingHelmDaily.groovy
+++ .ci/e2eTestingHelmDaily.groovy
@@ -37,28 +37,12 @@ pipeline {
   triggers {
     cron('H H(4-5) * * 1-5')
   }
-  parameters {
-    choice(name: 'LOG_LEVEL', choices: ['INFO', 'DEBUG'], description: 'Log level to be used')
-    booleanParam(name: "forceSkipGitChecks", defaultValue: true, description: "If it's needed to check for Git changes to filter by modified sources")
-    string(name: 'HELM_CHART_VERSION', defaultValue: '7.6.1', description: 'SemVer version of Helm chart to be used.')
-    string(name: 'HELM_VERSION', defaultValue: '2.16.3', description: 'SemVer version of Helm to be used.')
-    string(name: 'KIND_VERSION', defaultValue: '0.7.0', description: 'SemVer version of Kind to be used.')
-    string(name: 'KUBERNETES_VERSION', defaultValue: '1.15.3', description: 'SemVer version of Kubernetes to be used.')
-    string(name: 'GO_VERSION', defaultValue: '1.13.4', description: "Go version to use.")
-  }
   stages {
     stage('Run Tests') {
       steps {
         build(job: 'e2e-tests/e2e-testing-mbp/master',
           parameters: [
-            string(name: 'runTestsSuite', value: 'helm'),
-            string(name: 'LOG_LEVEL', value: "${params.LOG_LEVEL.trim()}"),
-            string(name: 'forceSkipGitChecks', value: "${params.forceSkipGitChecks}"),
-            string(name: 'HELM_CHART_VERSION', value: "${params.HELM_CHART_VERSION.trim()}"),
-            string(name: 'HELM_VERSION', value: "${params.HELM_VERSION.trim()}"),
-            string(name: 'KIND_VERSION', value: "${params.KIND_VERSION.trim()}"),
-            string(name: 'KUBERNETES_VERSION', value: "${params.KUBERNETES_VERSION.trim()}"),
-            string(name: 'GO_VERSION', value: "${params.GO_VERSION.trim()}")
+            string(name: 'runTestsSuite', value: 'helm')
           ],
           propagate: false,
           wait: false
--- .ci/e2eTestingIntegrationsDaily.groovy
+++ .ci/e2eTestingIntegrationsDaily.groovy
@@ -37,26 +37,12 @@ pipeline {
   triggers {
     cron('H H(4-5) * * 1-5')
   }
-  parameters {
-    choice(name: 'LOG_LEVEL', choices: ['INFO', 'DEBUG'], description: 'Log level to be used')
-    booleanParam(name: "forceSkipGitChecks", defaultValue: true, description: "If it's needed to check for Git changes to filter by modified sources")
-    string(name: 'METRICBEAT_VERSION', defaultValue: '7.8.0', description: 'SemVer version of the metricbeat to be used.')
-    choice(name: 'RETRY_TIMEOUT', choices: ['3', '5', '7', '11'], description: 'Max number of minutes for timeout backoff strategies')
-    string(name: 'STACK_VERSION', defaultValue: '7.8.0', description: 'SemVer version of the stack to be used.')
-    string(name: 'GO_VERSION', defaultValue: '1.13.4', description: "Go version to use.")
-  }
   stages {
     stage('Run Tests') {
       steps {
         build(job: 'e2e-tests/e2e-testing-mbp/master',
           parameters: [
-            string(name: 'runTestsSuite', value: 'metricbeat'),
-            string(name: 'LOG_LEVEL', value: "${params.LOG_LEVEL.trim()}"),
-            string(name: 'forceSkipGitChecks', value: "${params.forceSkipGitChecks}"),
-            string(name: 'METRICBEAT_VERSION', value: "${params.METRICBEAT_VERSION.trim()}"),
-            string(name: 'RETRY_TIMEOUT', value: "${params.RETRY_TIMEOUT.trim()}"),
-            string(name: 'STACK_VERSION', value: "${params.STACK_VERSIONSTACK_VERSION.trim()}"),
-            string(name: 'GO_VERSION', value: "${params.GO_VERSION.trim()}")
+            string(name: 'runTestsSuite', value: 'metricbeat')
           ],
           propagate: false,
           wait: false
