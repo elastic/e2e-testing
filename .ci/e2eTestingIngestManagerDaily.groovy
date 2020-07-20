c6a5c559432047031815886a2269ca5f464aef4e
Manuel de la Peña <mdelapenya@gmail.com>
2020-07-21 00:07:28 +0200

    fix: do not check for modifications in the downstream job

--- .ci/e2eTestingIngestManagerDaily.groovy
+++ .ci/e2eTestingIngestManagerDaily.groovy
@@ -42,6 +42,7 @@ pipeline {
       steps {
         build(job: 'e2e-tests/e2e-testing-mbp/master',
           parameters: [
+            booleanParam(name: 'forceSkipGitChecks', value: true)
             string(name: 'runTestsSuite', value: 'ingest-manager')
           ],
           propagate: false,
