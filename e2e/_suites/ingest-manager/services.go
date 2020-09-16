ebec495e323b36ccffb02a237dc64bc9604fcafe
Manuel de la Peña <mdelapenya@gmail.com>
2020-09-16 18:44:33 +0200

    chore: update GCP bucket calculation (#306)
    * fix: normalise GCP bucket structure
    
    * chore: extract agent download logic from the helper method
    
    * chore: simplify
    
    * feat: support pagination when retrieving objects from a GCP bucket
    
    * chore: explain we reached the end of the list
    # Conflicts:
    #	e2e/utils.go

--- e2e/_suites/ingest-manager/services.go
+++ e2e/_suites/ingest-manager/services.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/elastic/e2e-testing/cli/config"
 	"github.com/elastic/e2e-testing/cli/shell"
 	"github.com/elastic/e2e-testing/e2e"
 	log "github.com/sirupsen/logrus"
 )
+
+const agentVersionBase = "7.9.1"
 
 // agentVersion is the version of the agent to use
 // It can be overriden by ELASTIC_AGENT_VERSION env var
@@ -16,7 +20,7 @@ var agentVersion = "7.9.1"
 func init() {
 	config.Init()
 
-	agentVersion = shell.GetEnv("ELASTIC_AGENT_VERSION", agentVersion)
+	agentVersion = shell.GetEnv("ELASTIC_AGENT_VERSION", agentVersionBase)
 }
 
 // ElasticAgentInstaller represents how to install an agent, depending of the box type
@@ -66,13 +70,53 @@ func (i *ElasticAgentInstaller) getElasticAgentLogs(hostname string) error {
 
 // downloadAgentBinary it downloads the binary and stores the location of the downloaded file
 // into the installer struct, to be used else where
-func downloadAgentBinary(artifact string, version string, os string, arch string, extension string) (string, string, error) {
-	downloadURL, err := e2e.GetElasticArtifactURL(artifact, version, os, arch, extension)
+// If the environment variable ELASTIC_AGENT_DOWNLOAD_URL exists, then the artifact to be downloaded will
+// be defined by that value
+// Else, if the environment variable ELASTIC_AGENT_USE_CI_SNAPSHOTS is set, then the artifact
+// to be downloaded will be defined by the latest snapshot produced by the Beats CI.
+func downloadAgentBinary(artifact string, version string, OS string, arch string, extension string) (string, string, error) {
+	fileName := fmt.Sprintf("%s-%s-%s.%s", artifact, version, arch, extension)
+
+	if downloadURL, exists := os.LookupEnv("ELASTIC_AGENT_DOWNLOAD_URL"); exists {
+		filePath, err := e2e.DownloadFile(downloadURL)
+
+		return fileName, filePath, err
+	}
+
+	var downloadURL string
+	var err error
+
+	useCISnapshots, _ := shell.GetEnvBool("ELASTIC_AGENT_USE_CI_SNAPSHOTS")
+	if useCISnapshots {
+		log.Debug("Using CI snapshots for the Elastic Agent")
+
+		// We will use the snapshots produced by Beats CI
+		bucket := "beats-ci-artifacts"
+		object := fmt.Sprintf("snapshots/%s", fileName)
+
+		// we are setting a version from a pull request: the version of the artifact will be kept as the base one
+		// i.e. /pull-requests/pr-21100/elastic-agent-7.9.1-x86_64.rpm
+		// i.e. /pull-requests/pr-21100/elastic-agent-7.9.1-amd64.deb
+		if strings.HasPrefix(version, "pr-") {
+			log.WithField("PR", version).Debug("Using CI snapshots a pull request")
+			object = fmt.Sprintf("pull-requests/%s/%s/%s", version, artifact, fileName)
+		}
+
+		downloadURL, err = e2e.GetObjectURLFromBucket(bucket, object)
+		if err != nil {
+			return "", "", err
+		}
+
+		filePath, err := e2e.DownloadFile(downloadURL)
+
+		return fileName, filePath, err
+	}
+
+	downloadURL, err = e2e.GetElasticArtifactURL(artifact, agentVersionBase, OS, arch, extension)
 	if err != nil {
 		return "", "", err
 	}
 
-	fileName := fmt.Sprintf("%s-%s-%s-%s.%s", artifact, version, os, arch, extension)
 	filePath, err := e2e.DownloadFile(downloadURL)
 
 	return fileName, filePath, err
