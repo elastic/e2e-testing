f72c32d580397e10cb42b054fc25fea281478117
Manuel de la Peña <mdelapenya@gmail.com>
2020-09-29 17:34:25 +0200

    chore: rename test

--- cli/services/kibana_test.go
+++ cli/services/kibana_test.go
@@ -13,19 +13,19 @@ func TestNewClient(t *testing.T) {
 	assert.Equal(t, "http://localhost:5601", client.getURL())
 }
 
-func TestNewClientWithPathStartingWithSlash(t *testing.T) {
+func TestNewKibanaClientWithPathStartingWithSlash(t *testing.T) {
 	client := NewKibanaClient().withURL("/with_slash")
 	assert.NotNil(t, client)
 	assert.Equal(t, "http://localhost:5601/with_slash", client.getURL())
 }
 
-func TestNewClientWithPathStartingWithoutSlash(t *testing.T) {
+func TestNewKibanaClientWithPathStartingWithoutSlash(t *testing.T) {
 	client := NewKibanaClient().withURL("without_slash")
 	assert.NotNil(t, client)
 	assert.Equal(t, "http://localhost:5601/without_slash", client.getURL())
 }
 
-func TestNewClientWithMultiplePathsKeepsLastOne(t *testing.T) {
+func TestNewKibanaClientWithMultiplePathsKeepsLastOne(t *testing.T) {
 	client := NewKibanaClient().withURL("/with_slash").withURL("lastOne")
 	assert.NotNil(t, client)
 	assert.Equal(t, "http://localhost:5601/lastOne", client.getURL())
