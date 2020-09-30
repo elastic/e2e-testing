d8dc81b71002775a987d46d224232e9dfe832b06
Manuel de la Peña <mdelapenya@gmail.com>
2020-09-30 11:42:36 +0200

    chore: add first contract test for the provider
    We are creating it here, instead of in Kibana for simplicity

---
+++ cli/services/kibana_provider_test.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/pact-foundation/pact-go/types"
+)
+
+// The Provider verification
+func TestPactProvider(t *testing.T) {
+	pact := createPact()
+
+	// Verify the Provider - Tag-based Published Pacts for any known consumers
+	_, err := pact.VerifyProvider(t, types.VerifyRequest{
+		ProviderBaseURL:    fmt.Sprintf("http://127.0.0.1:%d", 5601),
+		Tags:               []string{"7.9.1"},
+		FailIfNoPactsFound: false,
+		// Use this if you want to test without the Pact Broker
+		PactURLs:                   []string{filepath.FromSlash(fmt.Sprintf("%s/e2e_testing_framework-fleet.json", os.Getenv("PACT_DIR")))},
+		PublishVerificationResults: true,
+		ProviderVersion:            "7.9.1",
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
