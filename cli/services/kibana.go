6b50357507974f32462402a708af1b5509907fc3
Manuel de la Peña <mdelapenya@gmail.com>
2020-10-01 13:24:32 +0200

    chore: move waitForKibana method to the client

--- cli/services/kibana.go
+++ cli/services/kibana.go
@@ -7,7 +7,9 @@ package services
 import (
 	"fmt"
 	"strings"
+	"time"
 
+	backoff "github.com/cenkalti/backoff/v4"
 	curl "github.com/elastic/e2e-testing/cli/shell"
 	log "github.com/sirupsen/logrus"
 )
@@ -227,6 +229,73 @@ func (k *KibanaClient) UpdateIntegrationPackageConfig(packageConfigID string, pa
 	}
 
 	return body, err
+}
+
+// WaitForKibana waits for kibana running in localhost:5601 to be healthy, returning false
+// if kibana does not get healthy status in a defined number of minutes.
+func (k *KibanaClient) WaitForKibana(maxTimeoutMinutes time.Duration) (bool, error) {
+	return k.WaitForKibanaFromHostPort("localhost", 5601, maxTimeoutMinutes)
+}
+
+// WaitForKibanaFromHostPort waits for kibana running in a host:port to be healthy, returning false
+// if kibana does not get healthy status in a defined number of minutes.
+func (k *KibanaClient) WaitForKibanaFromHostPort(host string, port int, maxTimeoutMinutes time.Duration) (bool, error) {
+	k.baseURL = fmt.Sprintf("http://%s:%d", host, port)
+	k.withURL("/status")
+
+	var (
+		initialInterval     = 500 * time.Millisecond
+		randomizationFactor = 0.5
+		multiplier          = 2.0
+		maxInterval         = 5 * time.Second
+		maxElapsedTime      = maxTimeoutMinutes
+	)
+
+	exp := backoff.NewExponentialBackOff()
+	exp.InitialInterval = initialInterval
+	exp.RandomizationFactor = randomizationFactor
+	exp.Multiplier = multiplier
+	exp.MaxInterval = maxInterval
+	exp.MaxElapsedTime = maxElapsedTime
+
+	retryCount := 1
+
+	kibanaStatus := func() error {
+		r := curl.HTTPRequest{
+			BasicAuthUser:     "elastic",
+			BasicAuthPassword: "changeme",
+			URL:               k.getURL(),
+		}
+
+		_, err := curl.Get(r)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error":          err,
+				"retry":          retryCount,
+				"statusEndpoint": r.URL,
+				"elapsedTime":    exp.GetElapsedTime(),
+			}).Warn("The Kibana instance is not healthy yet")
+
+			retryCount++
+
+			return err
+		}
+
+		log.WithFields(log.Fields{
+			"retries":        retryCount,
+			"statusEndpoint": r.URL,
+			"elapsedTime":    exp.GetElapsedTime(),
+		}).Info("The Kibana instance is healthy")
+
+		return nil
+	}
+
+	err := backoff.Retry(kibanaStatus, exp)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
 
 // createDefaultHTTPRequest Creates a default HTTP request, including the basic auth,
