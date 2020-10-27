93de3e48550bb7e6fd5c8af258a12915a82056af
Manuel de la Peña <mdelapenya@gmail.com>
2020-10-27 12:31:28 +0100

    chore: wait 30 seconds before starting metricbeat
    This will allow the integration target service to start-up, so metricbeat
    won't find errors related to service not being available

--- e2e/_suites/metricbeat/metricbeat_test.go
+++ e2e/_suites/metricbeat/metricbeat_test.go
@@ -282,6 +282,10 @@ func (mts *MetricbeatTestSuite) installedUsingConfiguration(configuration string
 
 // runMetricbeatService runs a metricbeat service entity for a service to monitor it
 func (mts *MetricbeatTestSuite) runMetricbeatService() error {
+	// this is needed because, in general, the target service (apache, mysql, redis) does not have a healthcheck
+	waitForService := time.Duration(timeoutFactor) * 10
+	e2e.Sleep(fmt.Sprintf("%d", waitForService))
+
 	serviceManager := services.NewServiceManager()
 
 	logLevel := log.GetLevel().String()
