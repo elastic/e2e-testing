1439302cec0f3279d68adcdb19355c2c07171436
Manuel de la Peña <mdelapenya@gmail.com>
2021-01-14 20:40:28 +0100

    chore: store container logs in Jenkins (#577) (#591)
    * chore: use filebeat to store container logs in a separate file
    
    * chore: simplify method not returning error
    
    We are not handling it at any place, so let's remove it
    
    * chore: print metricbeat logs only in developer mode
    
    * chore: do not fail if metricbeat logs are not retrieved
    
    * fix: separate docker logs per test suite and tags

--- e2e/_suites/helm/helm_charts_test.go
+++ e2e/_suites/helm/helm_charts_test.go
@@ -40,7 +40,7 @@ var kubectl k8s.Kubectl
 func init() {
 	config.Init()
 
-	developerMode, _ = shell.GetEnvBool("DEVELOPER_MODE")
+	developerMode = shell.GetEnvBool("DEVELOPER_MODE")
 	if developerMode {
 		log.Info("Running in Developer mode 💻: runtime dependencies between different test runs will be reused to speed up dev cycle")
 	}
