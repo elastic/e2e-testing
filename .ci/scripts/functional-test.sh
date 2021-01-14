9c36a85597f28f0748bff497f01c87b70c3e4b41
Manuel de la Peña <mdelapenya@gmail.com>
2021-01-14 17:36:21 +0100

    fix: no need of tee

--- .ci/scripts/functional-test.sh
+++ .ci/scripts/functional-test.sh
@@ -31,7 +31,7 @@ REPORT="outputs/TEST-${SUITE}"
 ## Generate test report even if make failed.
 set +e
 exit_status=0
-if ! SUITE=${SUITE} TAGS="${TAGS}" FORMAT=junit:${REPORT}.xml STACK_VERSION=${STACK_VERSION} METRICBEAT_VERSION=${METRICBEAT_VERSION} make --no-print-directory -C e2e functional-test | tee ${REPORT}  ; then
+if ! SUITE=${SUITE} TAGS="${TAGS}" FORMAT=junit:${REPORT}.xml STACK_VERSION=${STACK_VERSION} METRICBEAT_VERSION=${METRICBEAT_VERSION} make --no-print-directory -C e2e functional-test ; then
   echo 'ERROR: functional-test failed'
   exit_status=1
 fi
