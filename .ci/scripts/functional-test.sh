b290b176a96e6cf79bb9ed58f04b1bba4d152176
Manuel de la Peña <mdelapenya@gmail.com>
2021-01-20 23:17:53 +0100

    fix: let godog handle the creation of the junit test file (#629) (#632)
    It was causing that, after a test failure, the script exited with code 1,
    and possibly not allowing next stages to fetch the generated files.

--- .ci/scripts/functional-test.sh
+++ .ci/scripts/functional-test.sh
@@ -28,12 +28,4 @@ mkdir -p outputs
 
 REPORT="$(pwd)/outputs/TEST-${SUITE}"
 
-## Generate test report even if make failed.
-set +e
-exit_status=0
-if ! SUITE=${SUITE} TAGS="${TAGS}" FORMAT=junit:${REPORT}.xml STACK_VERSION=${STACK_VERSION} METRICBEAT_VERSION=${METRICBEAT_VERSION} make --no-print-directory -C e2e functional-test ; then
-  echo 'ERROR: functional-test failed'
-  exit_status=1
-fi
-
-exit $exit_status
+SUITE=${SUITE} TAGS="${TAGS}" FORMAT=junit:${REPORT}.xml STACK_VERSION=${STACK_VERSION} METRICBEAT_VERSION=${METRICBEAT_VERSION} make --no-print-directory -C e2e functional-test
