e8db9e98da75af70ac6aca085e49427e89d98824
Manuel de la Peña <mdelapenya@gmail.com>
2021-02-03 16:10:12 +0100

    feat: support using local Beats repository in the tests (#688)
    * feat: use local beats directory for elastic-agent installers
    
    * docs: document BEATS_LOCAL_PATH variable
    
    * chore: check that the local file exists
    
    * chore: add unit tests for the BEATS_LOCAL_PATH variable
    
    * chore: simplify logic after test coverage
    
    * chore: enrich test name
    
    * chore: rename test
    
    * chore: move default values to the end of the descriptions
    
    * docs: clarify local path for Beats

--- e2e/_suites/fleet/services_test.go
+++ e2e/_suites/fleet/services_test.go
@@ -2,12 +2,74 @@ package main
 
 import (
 	"os"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 var testVersion = agentVersionBase
+
+func TestDownloadAgentBinary(t *testing.T) {
+	artifact := "elastic-agent"
+	beatsDir := path.Join("..", "..", "_testresources", "beats")
+	distributionsDir := path.Join(beatsDir, "x-pack", "elastic-agent", "build", "distributions")
+	OS := "linux"
+	version := "8.0.0-SNAPSHOT"
+
+	t.Run("Fetching non-existent binary from local Beats dir throws an error", func(t *testing.T) {
+		defer os.Unsetenv("BEATS_LOCAL_PATH")
+		os.Setenv("BEATS_LOCAL_PATH", beatsDir)
+
+		arch := "foo_arch"
+		extension := "foo_ext"
+
+		_, _, err := downloadAgentBinary(artifact, version, OS, arch, extension)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("Fetching RPM binary from local Beats dir", func(t *testing.T) {
+		defer os.Unsetenv("BEATS_LOCAL_PATH")
+		os.Setenv("BEATS_LOCAL_PATH", beatsDir)
+
+		arch := "x86_64"
+		extension := "rpm"
+		expectedFileName := "elastic-agent-8.0.0-SNAPSHOT-x86_64.rpm"
+
+		newFileName, downloadedFilePath, err := downloadAgentBinary(artifact, version, OS, arch, extension)
+		assert.Nil(t, err)
+		assert.Equal(t, newFileName, expectedFileName)
+		assert.Equal(t, downloadedFilePath, path.Join(distributionsDir, expectedFileName))
+	})
+
+	t.Run("Fetching DEB binary from local Beats dir", func(t *testing.T) {
+		defer os.Unsetenv("BEATS_LOCAL_PATH")
+		os.Setenv("BEATS_LOCAL_PATH", beatsDir)
+
+		arch := "amd64"
+		extension := "deb"
+		expectedFileName := "elastic-agent-8.0.0-SNAPSHOT-amd64.deb"
+
+		newFileName, downloadedFilePath, err := downloadAgentBinary(artifact, version, OS, arch, extension)
+		assert.Nil(t, err)
+		assert.Equal(t, newFileName, expectedFileName)
+		assert.Equal(t, downloadedFilePath, path.Join(distributionsDir, expectedFileName))
+	})
+
+	t.Run("Fetching TAR binary from local Beats dir", func(t *testing.T) {
+		defer os.Unsetenv("BEATS_LOCAL_PATH")
+		os.Setenv("BEATS_LOCAL_PATH", beatsDir)
+
+		arch := "amd64"
+		extension := "tar.gz"
+		expectedFileName := "elastic-agent-8.0.0-SNAPSHOT-linux-amd64.tar.gz"
+
+		newFileName, downloadedFilePath, err := downloadAgentBinary(artifact, version, OS, arch, extension)
+		assert.Nil(t, err)
+		assert.Equal(t, newFileName, expectedFileName)
+		assert.Equal(t, downloadedFilePath, path.Join(distributionsDir, expectedFileName))
+	})
+}
 
 func TestGetGCPBucketCoordinates_Commits(t *testing.T) {
 	artifact := "elastic-agent"
