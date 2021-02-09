04f12cc07f5d58cb4ec216aeb4f9d83a2931ecc5
Victor Martinez <victormartinezrubio@gmail.com>
2021-02-09 17:27:32 +0000

    Metricbeat goal (#705) backport for 7.10.x (#716)
    * Metricbeat goal (#705)
    * Update .ci/scripts/metricbeat-test.sh

---
+++ .ci/scripts/metricbeat-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+## Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+## or more contributor license agreements. Licensed under the Elastic License;
+## you may not use this file except in compliance with the Elastic License.
+
+set -euxo pipefail
+#
+# Run the functional tests for metricbeat using the functional-test wrapper
+#
+# Parameters:
+#   - STACK_VERSION - that's the version of the stack to be tested. Default '8.0.0-SNAPSHOT'.
+#   - METRICBEAT_VERSION - that's the version of the metricbeat to be tested. Default '8.0.0-SNAPSHOT'.
+#
+
+STACK_VERSION=${1:-'7.10-SNAPSHOT'}
+METRICBEAT_VERSION=${2:-'7.10-SNAPSHOT'}
+SUITE='metricbeat'
+
+.ci/scripts/functional-test.sh "${SUITE}" "" "${STACK_VERSION}" "${METRICBEAT_VERSION}"
