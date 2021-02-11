a4f29c94e112c27e4d755057fd7d2a87ea5cdf85
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-02-11 14:20:11 +0000

    Revert "Add CI resilience with the script itself"
    This reverts commit 49c469a8eb09ac23f9647bf755db59a716e920d3.

--- .ci/scripts/install-dependencies.sh
+++ .ci/scripts/install-dependencies.sh
@@ -9,30 +9,10 @@ set -euxo pipefail
 # Install the dependencies using the install and test make goals.
 #
 
-function installDependenciesForThePreCommit() {
-    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.34.1
-    go get -v golang.org/x/lint/golint
-}
+# Install some other dependencies required for the pre-commit
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.34.1
 
-function install() {
-    CI_UTILS=/usr/local/bin/bash_standard_lib.sh
-    if [ -e "${CI_UTILS}" ] ; then
-        set +u
-        if [ "$sourced" != "1" ] ; then
-            # shellcheck disable=SC1090
-            source "${CI_UTILS}"
-            sourced=1
-        fi
-        set -u
-        # shellcheck disable=SC2086
-        retry 3 $1 # It is a function call
-    else
-        $1
-    fi
-}
-
-# Install required dependencies with some retry
-install installDependenciesForThePreCommit
+go get -v golang.org/x/lint/golint
 
 # Install project dependencies
-install make -C cli install
+make -C cli install
