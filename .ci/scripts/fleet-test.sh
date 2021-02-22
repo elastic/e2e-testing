08425cc2fe61ea642a9e6b2b55f2a4e4398b081e
Victor Martinez <victormartinezrubio@gmail.com>
2021-02-22 09:28:01 +0000

    Fleet goal (#784)

---
+++ .ci/scripts/fleet-test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+## Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+## or more contributor license agreements. Licensed under the Elastic License;
+## you may not use this file except in compliance with the Elastic License.
+
+set -euxo pipefail
+#
+# Run the functional tests for fleets using the functional-test wrapper
+#
+# Parameters:
+#   - STACK_VERSION - that's the version of the stack to be tested. Default '8.0.0-SNAPSHOT'.
+#
+
+STACK_VERSION=${1:-'8.0.0-SNAPSHOT'}
+SUITE='fleet'
+
+.ci/scripts/functional-test.sh "${SUITE}" "" "${STACK_VERSION}"
