2132b6df84114c7f16c8876cf4e0122ec999fb23
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-03-04 10:51:14 +0000

    Remove the debug

--- .ci/scripts/metrics.sh
+++ .ci/scripts/metrics.sh
@@ -4,7 +4,7 @@
 ## or more contributor license agreements. Licensed under the Elastic License;
 ## you may not use this file except in compliance with the Elastic License.
 
-set -xe
+set -e
 
 FILE_BRANCHES='branches.txt'
 FILE_PRS='prs.txt'
