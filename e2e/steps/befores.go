9d71b2f3947f8030676d4ac65b7add9213a40973
Manuel de la Peña <mdelapenya@gmail.com>
2021-03-08 17:37:58 +0100

    feat: instrument Helm charts test suite (#858)
    * chore: add a span for waiting for elasticsearch
    
    * chore: add a span for waiting for Kibana
    
    * chore: add spans for Running/Stopping docker compose
    
    * chore: log file name when loading a docker image from TAR
    
    * chore: extract init APM dependencies to a steps package
    
    * fix: enrich log for local APM services
    
    * chore: enrich log for starting compose files
    
    * fix: consistent span type for compose services
    
    * chore: do not initialise helm test suite with Go init
    
    * chore: pass context to shell.Execute
    
    * feat: instrument helm charts test suite
    
    * fix: use passed context

---
+++ e2e/steps/befores.go
@@ -0,0 +1,41 @@
+package steps
+
+import (
+	"context"
+	"strings"
+
+	"github.com/elastic/e2e-testing/cli/services"
+	"github.com/elastic/e2e-testing/cli/shell"
+	log "github.com/sirupsen/logrus"
+)
+
+// AddAPMServicesForInstrumentation adds a Kibana and APM Server instances to the running project
+func AddAPMServicesForInstrumentation(ctx context.Context, profile string, stackVersion string, needsKibana bool, env map[string]string) {
+	serviceManager := services.NewServiceManager()
+
+	apmServerURL := shell.GetEnv("APM_SERVER_URL", "")
+	if strings.HasPrefix(apmServerURL, "http://localhost") {
+		apmServices := []string{
+			"apm-server",
+		}
+
+		if needsKibana {
+			env["kibanaTag"] = stackVersion
+			apmServices = append(apmServices, "kibana")
+		}
+
+		log.WithFields(log.Fields{
+			"services": apmServices,
+			"version":  stackVersion,
+		}).Info("Starting local APM services for instrumentation")
+
+		env["apmServerTag"] = stackVersion
+		err := serviceManager.AddServicesToCompose(ctx, profile, apmServices, env)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+				"env":   env,
+			}).Warn("The local APM Server and Kibana could not be started, but they are not needed by the tests. Continuing")
+		}
+	}
+}
