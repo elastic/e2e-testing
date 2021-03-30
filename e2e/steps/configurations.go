675b56be905d7d6cb3f7e13013f197bf5bd1bd6b
Manuel de la Peña <mdelapenya@gmail.com>
2021-03-30 08:42:58 +0200

    fix: use PRs configuration files (#956) backport for 7.12.x (#965)
    * fix: use PRs configuration files (#956)
    
    * chore: download config files from the right commit
    
    * fix: return our Docker namespace for local images too
    
    * chore: use trace level
    
    * chore: extract fetch config file to a method
    
    * fix: wrong copy&paste
    
    * chore: use maintenance branch

---
+++ e2e/steps/configurations.go
@@ -0,0 +1,60 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package steps
+
+import (
+	"path"
+
+	"github.com/elastic/e2e-testing/cli/shell"
+	"github.com/elastic/e2e-testing/e2e"
+	log "github.com/sirupsen/logrus"
+)
+
+// FetchBeatConfiguration where 'configFileName' represents the configuration file, including extension.
+// It will read environment to determine if it must read a file from the local file system, or in the
+// contrary, download a file from Github. In the latter case, it will use a commit of the maintenance branch
+// used in the tests
+func FetchBeatConfiguration(xpack bool, beat string, configFileName string) (string, error) {
+	beatsLocalPath := shell.GetEnv("BEATS_LOCAL_PATH", "")
+
+	if beatsLocalPath != "" {
+		configurationFilePath := beatsLocalPath
+		if xpack {
+			configurationFilePath = path.Join(beatsLocalPath, "x-pack")
+		}
+		configurationFilePath = path.Join(beatsLocalPath, beat, configFileName)
+
+		log.WithFields(log.Fields{
+			"beat":  beat,
+			"file":  configurationFilePath,
+			"xpack": xpack,
+		}).Trace("Reading configuration file from local path")
+
+		return configurationFilePath, nil
+	}
+
+	refspec := shell.GetEnv("GITHUB_CHECK_SHA1", "7.12")
+
+	configurationFileURL := "https://raw.githubusercontent.com/elastic/beats/" + refspec
+	if xpack {
+		configurationFileURL += "/x-pack"
+	}
+
+	configurationFileURL += "/" + beat + "/" + configFileName
+
+	configurationFilePath, err := e2e.DownloadFile(configurationFileURL)
+	if err != nil {
+		return "", err
+	}
+
+	log.WithFields(log.Fields{
+		"beat":  beat,
+		"path":  configurationFilePath,
+		"url":   configurationFileURL,
+		"xpack": xpack,
+	}).Trace("Configuration file downloaded from Github")
+
+	return configurationFilePath, nil
+}
