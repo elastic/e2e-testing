326e46de9407a0aa840ed1c3b6dd1b5780a3bf6f
Manuel de la Peña <mdelapenya@gmail.com>
2021-03-30 17:06:30 +0200

    chore: do not notify on slack on build greens

--- .ci/e2eKibana.groovy
+++ .ci/e2eKibana.groovy
@@ -97,7 +97,7 @@ def runE2ETests(String suite) {
   def parameters = [
     booleanParam(name: 'forceSkipGitChecks', value: true),
     booleanParam(name: 'forceSkipPresubmit', value: true),
-    booleanParam(name: 'notifyOnGreenBuilds', value: !isPR()),
+    booleanParam(name: 'notifyOnGreenBuilds', value: false),
     booleanParam(name: 'BEATS_USE_CI_SNAPSHOTS', value: true),
     string(name: 'runTestsSuites', value: suite),
     string(name: 'GITHUB_CHECK_NAME', value: env.GITHUB_CHECK_E2E_TESTS_NAME),
