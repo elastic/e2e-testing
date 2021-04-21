5f59670924df6deaa8d37e59e507c155ba6db529
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-04-21 11:22:36 -0400

    v2 refactor (#1008)
    * Move kibana into internals, update fleet test suite
    * migrate docker-compose related code to internal layout
    * move docker related code to internal layout
    * move git related code to internal layout
    * move common attributes into internal common file system layout
    * move elasticsearch specifics into its own filesystem layout
    * move installer based code to internal layout
    * move shell related code to internal layout
    * move sanitizer code to internal layout
    * move io related code to internal layout
    * move utils into internal layout
    * Update package integration querying/altering
    * move curl to internal layout
    * move helm to internal layout
    * move kubectl into internal layout
    * move state internal filesystem
    * cleanup config in stand-alone
    * remove unused files
    * Uniquify the stand-alone step for checking agent status (#993)
    
    There were 2 steps identical in both the stand-alone and fleet test suites.
    Running the stand-alone test suite was picking up the step from the fleet test
    suite and trying to reference the FleetTestSuite structure which did not hold
    any of the agent information (like the hostname) for the stand alone tests.
    
    This fixes it so that the standalone test step is being referenced in the
    correct test suite.
    
    * Remove the agent config file parameters for stand alone (#983)
    * Update helm/metricbeat tests to use new layout
    * Fix policy endpoint update
    * fix panic on helm init
    * Fix step reference as this being merged seperately
    * Update function call to correct standalone step
    * Fix merge conflict
    * update ProfileEnv query/set for KibanaVersion
    * More fixes to agent endpoint security checks
    * update backend feature to call out endpoint in step
    * use common.TimeoutFactor in docker checkprocess state
    * Update adding endpoint integration
    * enable features for fleet server
    * not necessary to enroll after install
    * wait for filebeat/metricbeat before restarts
    * clear out fts.CurrentToken during beforeScenario
    * attach system integration on deploy
    * enroll if rpm
    * dont store fleet policy
    * update kibana config for latest fleet server
    * Update e2e/_suites/fleet/fleet.go
    * Update e2e/_suites/fleet/fleet.go
    * Update e2e/_suites/fleet/fleet.go
    * Update .pre-commit-config.yaml
    * Update e2e/Makefile
    * rename apt -> deb for installer type
    * execute docker start/stop with timeout between
    * fixes fleet_server scenario
    * Utilize fleet server in all tests
    * Fix enrollment url for fleet server
    * Query elasticsearch logs for endpoint security event changes
    * Increase search result size for ES
    * Fix issue with fleet server restarting continuously
    * unpin kibana pr now that most major breakage is resolved
    * force unenroll
    * for new fleet bootstrap on re-enrollment
    * Fix unenrollment
    * Add timeout safeguard to elastic-agent execution
    
    In some cases such as attempting to re-enroll with a revoked token, the
    elastic-agent will retry indefinitely. This fix adds a safeguard utilizing
    'timeout' command prepended to the elastic-agent command so that it will timeout
    after TimeoutFactor
    
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>
    Co-authored-by: Manuel de la Peña <mdelapenya@gmail.com>

---
+++ internal/kibana/policies.go
@@ -0,0 +1,149 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package kibana
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+)
+
+// Policy represents an Ingest Manager policy.
+type Policy struct {
+	ID                   string `json:"id,omitempty"`
+	Name                 string `json:"name"`
+	Description          string `json:"description"`
+	Namespace            string `json:"namespace"`
+	IsDefault            bool   `json:"is_default"`
+	IsManaged            bool   `json:"is_managed"`
+	IsDefaultFleetServer bool   `json:"is_default_fleet_server"`
+	AgentsCount          int    `json:"agents"` // Number of agents connected to Policy
+	Status               string `json:"status"`
+}
+
+// GetDefaultPolicy gets the default policy or optionally the default fleet policy
+func (c *Client) GetDefaultPolicy(fleetServer bool) (Policy, error) {
+	policies, err := c.ListPolicies()
+	if err != nil {
+		return Policy{}, err
+	}
+
+	for _, policy := range policies {
+		if fleetServer && policy.IsDefaultFleetServer {
+			log.WithField("policy", policy).Trace("Returning Default Fleet Server Policy")
+			return policy, nil
+		} else if !fleetServer && policy.IsDefault {
+			log.WithField("policy", policy).Trace("Returning Default Agent Policy")
+			return policy, nil
+		}
+	}
+	return Policy{}, errors.New("Could not obtain default policy")
+}
+
+// ListPolicies returns the list of policies
+func (c *Client) ListPolicies() ([]Policy, error) {
+	statusCode, respBody, err := c.get(fmt.Sprintf("%s/agent_policies", FleetAPI))
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"body":  respBody,
+			"error": err,
+		}).Error("Could not get Fleet's policies")
+		return nil, err
+	}
+
+	if statusCode != 200 {
+		log.WithFields(log.Fields{
+			"error":      err,
+			"statusCode": statusCode,
+		}).Error("Could not get Fleet's policies")
+
+		return nil, err
+	}
+
+	var resp struct {
+		Items []Policy `json:"items"`
+	}
+	if err := json.Unmarshal(respBody, &resp); err != nil {
+		return nil, errors.Wrap(err, "Unable to convert list of policies to JSON")
+	}
+
+	return resp.Items, nil
+}
+
+// Var represents a single variable at the package or
+// data stream level, encapsulating the data type of the
+// variable and it's value.
+type Var struct {
+	Value interface{} `json:"value"`
+	Type  string      `json:"type"`
+}
+
+// Vars is a collection of variables either at the package or
+// data stream level.
+type Vars map[string]Var
+
+// DataStream represents a data stream within a package.
+type DataStream struct {
+	Type    string `json:"type"`
+	Dataset string `json:"dataset"`
+}
+
+// Input represents a package-level input.
+type Input struct {
+	Type    string        `json:"type"`
+	Enabled bool          `json:"enabled"`
+	Streams []interface{} `json:"streams"`
+	Vars    Vars          `json:"vars,omitempty"`
+	Config  interface{}   `json:"config,omitempty"`
+}
+
+// PackageDataStream represents a request to add a single package's single data stream to a
+// Policy in Ingest Manager.
+type PackageDataStream struct {
+	ID          string             `json:"id,omitempty"`
+	Name        string             `json:"name"`
+	Description string             `json:"description"`
+	Namespace   string             `json:"namespace"`
+	PolicyID    string             `json:"policy_id"`
+	Enabled     bool               `json:"enabled"`
+	OutputID    string             `json:"output_id"`
+	Inputs      []Input            `json:"inputs"`
+	Package     IntegrationPackage `json:"package"`
+}
+
+// ListPackagePolicies return list of package policies
+func (c *Client) ListPackagePolicies() ([]PackageDataStream, error) {
+	statusCode, respBody, err := c.get(fmt.Sprintf("%s/package_policies", FleetAPI))
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"body":  respBody,
+			"error": err,
+		}).Error("Could not get Fleet's package policies")
+		return nil, err
+	}
+
+	if statusCode != 200 {
+		log.WithFields(log.Fields{
+			"error":      err,
+			"statusCode": statusCode,
+		}).Error("Could not get Fleet's package policies")
+
+		return nil, err
+	}
+
+	var resp struct {
+		Items []PackageDataStream `json:"items"`
+	}
+
+	if err := json.Unmarshal(respBody, &resp); err != nil {
+		return nil, errors.Wrap(err, "Unable to convert list of package policies to JSON")
+	}
+
+	return resp.Items, nil
+}
