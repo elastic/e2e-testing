53a4bcb6e69da4d38febbf9016af10d414ba7f98
Manuel de la Peña <mdelapenya@gmail.com>
2021-04-26 17:45:24 +0200

    chore: add script to push the multiplatform manifest for centos and debian
    This script leverages infra's tool to write the manifest, which needs to
    be ran right after the images have been built and pushed. Therefore, the
    tool will write the manifest for both platforms (AMD/ARM), inspecting
    the existing platform-specific repositories, combining them into the target.
    
    FYI, the '-ARCH' placeholder will be replaced with the values in the
    '--platforms' argument

---
+++ .ci/scripts/push-multiplatform-manifest.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+## Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+## or more contributor license agreements. Licensed under the Elastic License;
+## you may not use this file except in compliance with the Elastic License.
+
+set -euxo pipefail
+
+readonly ELASTIC_REGISTRY="docker.elastic.co"
+readonly MANIFEST_TOOL_IMAGE="${ELASTIC_REGISTRY}/infra/manifest-tool:latest"
+readonly OBSERVABILITY_CI_REGISTRY="${ELASTIC_REGISTRY}/observability-ci"
+
+main() {
+  _push_multiplatform_manifest "centos"
+  _push_multiplatform_manifest "debian"
+}
+
+_push_multiplatform_manifest() {
+  local image="${1}"
+
+  local fqn="${OBSERVABILITY_CI_REGISTRY}/${image}-systemd:latest"
+  # the '-ARCH' placeholder will be replaced with the values in the '--platforms' argument
+  local templateFqn="${OBSERVABILITY_CI_REGISTRY}/${image}-systemd-ARCH:latest"
+
+  docker run --rm \
+    --mount src=${HOME}/.docker,target=/docker-config,type=bind \
+      ${MANIFEST_TOOL_IMAGE} --docker-cfg "/docker-config" \
+        push from-args \
+          --platforms linux/amd64,linux/arm64 \
+          --template ${templateFqn} \
+          --target ${fqn}
+}
+
+main "$@"
