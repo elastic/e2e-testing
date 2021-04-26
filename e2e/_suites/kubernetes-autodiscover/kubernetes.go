c52395dc53adf8ed95ff0187de82c86498d0abdc
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2021-04-26 11:47:31 +0200

    Kubernetes autodiscover suite (#1064)
    Adds a new suite to test Kubernetes autodiscover features.
    Reproduces some issues with short-living containers affecting Beats before 7.13
    and for what we didn't have tests coverage.
    
    It allows to define the scenarios using the gherkin language for feature files.
    Scenarios use templates that are mostly kubernetes manifests. A simple system of
    options is included to allow to select configuration blocks in these templates.
    
    It uses kubectl to interact with a kubernetes cluster. If no cluster is available, it
    starts one with kind. Each scenario is executed in a different namespace, namespace
    is destroyed after the scenario is run to clean all created resources.
    
    It currently works only with loads in a single namespace, in a single node, this is
    enough to test many autodiscover cases, but it could be extended in the future to cover
    multi-node or multi-namespace scenarios.
    
    Co-authored-by: Manuel de la Peña <mdelapenya@gmail.com>
    Co-authored-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

---
+++ e2e/_suites/kubernetes-autodiscover/kubernetes.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/elastic/e2e-testing/internal/common"
+	"github.com/elastic/e2e-testing/internal/shell"
+)
+
+type kubernetesControl struct {
+	config           string
+	Namespace        string
+	NamespaceUID     string
+	createdNamespace bool
+}
+
+func (c kubernetesControl) WithConfig(config string) kubernetesControl {
+	c.config = config
+	return c
+}
+
+func (c kubernetesControl) WithNamespace(ctx context.Context, namespace string) kubernetesControl {
+	if namespace == "" {
+		namespace = "test-" + uuid.New().String()
+		err := c.createNamespace(ctx, namespace)
+		if err != nil {
+			log.WithError(err).Fatalf("Failed to create namespace %s", namespace)
+		}
+		c.createdNamespace = true
+	}
+	uid, err := c.Run(ctx, "get", "namespace", namespace, "-o", "jsonpath={.metadata.uid}")
+	if err != nil {
+		log.WithError(err).Fatalf("Failed to get namespace %s uid", namespace)
+	}
+	c.NamespaceUID = uid
+	c.Namespace = namespace
+	return c
+}
+
+func (c kubernetesControl) createNamespace(ctx context.Context, namespace string) error {
+	if namespace == "" {
+		return nil
+	}
+
+	_, err := c.Run(ctx, "create", "namespace", namespace)
+	if err != nil {
+		return fmt.Errorf("namespace creation failed: %w", err)
+	}
+
+	// Wait for default account to be available, if not it is not possible to
+	// deploy pods in this namespace.
+	timeout := 60 * time.Second
+	exp := backoff.WithContext(common.GetExponentialBackOff(timeout), ctx)
+	return backoff.Retry(func() error {
+		_, err := c.Run(ctx, "get", "serviceaccount", "default")
+		if err != nil {
+			return fmt.Errorf("namespace was created but still not ready: %w", err)
+		}
+		return nil
+	}, exp)
+}
+
+func (c kubernetesControl) Cleanup(ctx context.Context) error {
+	if c.createdNamespace && c.Namespace != "" {
+		output, err := c.Run(ctx, "delete", "namespace", c.Namespace)
+		if err != nil {
+			return fmt.Errorf("failed to delete namespace %s: %v: %s", c.Namespace, err, output)
+		}
+	}
+	return nil
+}
+
+func (c kubernetesControl) Run(ctx context.Context, runArgs ...string) (output string, err error) {
+	return c.RunWithStdin(ctx, nil, runArgs...)
+}
+
+func (c kubernetesControl) RunWithStdin(ctx context.Context, stdin io.Reader, runArgs ...string) (output string, err error) {
+	shell.CheckInstalledSoftware("kubectl")
+	var args []string
+	if c.config != "" {
+		args = append(args, "--kubeconfig", c.config)
+	}
+	if c.Namespace != "" {
+		args = append(args, "--namespace", c.Namespace)
+	}
+	args = append(args, runArgs...)
+	return shell.ExecuteWithStdin(ctx, ".", stdin, "kubectl", args...)
+}
+
+type kubernetesCluster struct {
+	kindName   string
+	kubeconfig string
+
+	tmpDir string
+}
+
+func (c kubernetesCluster) Kubectl() kubernetesControl {
+	return kubernetesControl{}.WithConfig(c.kubeconfig)
+}
+
+func (c kubernetesCluster) isAvailable(ctx context.Context) error {
+	_, err := c.Kubectl().Run(ctx, "api-versions")
+	return err
+}
+
+func (c *kubernetesCluster) initialize(ctx context.Context) error {
+	err := c.isAvailable(ctx)
+	if err == nil {
+		return nil
+	}
+
+	log.Info("Kubernetes cluster not available, will start one using kind")
+	shell.CheckInstalledSoftware("kind")
+	kindVersion, err := shell.Execute(ctx, ".", "kind", "version")
+	if err != nil {
+		log.WithError(err).Fatal("Failed to get kind version")
+	}
+	log.Infof("Using %s", kindVersion)
+
+	c.tmpDir, err = ioutil.TempDir(os.TempDir(), "test-")
+	if err != nil {
+		log.WithError(err).Fatal("Failed to create temporary directory")
+	}
+
+	name := "kind-" + uuid.New().String()
+	c.kubeconfig = filepath.Join(c.tmpDir, "kubeconfig")
+
+	args := []string{
+		"create", "cluster",
+		"--name", name,
+		"--config", "testdata/kind.yml",
+		"--kubeconfig", c.kubeconfig,
+	}
+	if version, ok := os.LookupEnv("KUBERNETES_VERSION"); ok && version != "" {
+		log.Infof("Installing Kubernetes v%s", version)
+		args = append(args, "--image", "kindest/node:v"+version)
+	}
+	output, err := shell.Execute(ctx, ".", "kind", args...)
+	if err != nil {
+		log.WithError(err).WithField("output", output).Fatal("Failed to create kind cluster")
+		return err
+	}
+	c.kindName = name
+
+	log.Infof("Kubeconfig in %s", c.kubeconfig)
+
+	return nil
+}
+
+func (c *kubernetesCluster) cleanup(ctx context.Context) {
+	if c.kindName != "" {
+		_, err := shell.Execute(ctx, ".", "kind", "delete", "cluster", "--name", c.kindName)
+		if err != nil {
+			log.Warnf("Failed to delete kind cluster %s", c.kindName)
+		}
+		c.kindName = ""
+		log.Infof("kind cluster %s was deleted", c.kindName)
+	}
+	if c.tmpDir != "" {
+		err := os.RemoveAll(c.tmpDir)
+		if err != nil {
+			log.Warnf("Failed to remove temporary directory %s", c.tmpDir)
+		}
+	}
+}
