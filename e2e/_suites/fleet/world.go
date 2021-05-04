77a2c554e7c95842ed67592e494565951a9b5987
Juan Álvarez <juan.alvarez@elastic.co>
2021-05-04 19:19:56 +0200

    Unify fleet and stand-alone suites (#1112)
    * fix agent uninstall
    
    * unify fleet and stand alone suites
    
    * move things around a bit more
    
    * fixe bad merge
    
    * simplify some things

--- e2e/_suites/fleet/world.go
+++ e2e/_suites/fleet/world.go
@@ -13,8 +13,7 @@ import (
 
 // IngestManagerTestSuite represents a test suite, holding references to the pieces needed to run the tests
 type IngestManagerTestSuite struct {
-	Fleet      *FleetTestSuite
-	StandAlone *StandAloneTestSuite
+	Fleet *FleetTestSuite
 }
 
 func (imts *IngestManagerTestSuite) processStateOnTheHost(process string, state string) error {
@@ -22,7 +21,7 @@ func (imts *IngestManagerTestSuite) processStateOnTheHost(process string, state 
 
 	var containerName string
 
-	if imts.StandAlone.Hostname != "" {
+	if imts.Fleet.StandAlone {
 		containerName = fmt.Sprintf("%s_%s_%d", profile, common.ElasticAgentServiceName, 1)
 	} else {
 		agentInstaller := imts.Fleet.getInstaller()
