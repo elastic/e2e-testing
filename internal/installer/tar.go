ac300e265e1e4f0ec491aca397aee8dab222bd2e
Manuel de la Peña <mdelapenya@gmail.com>
2021-05-06 17:21:15 +0200

    Revert "chore: do not clean up installer's workspace"
    This reverts commit 91045012189c8868671973e6694595c751885782.

--- internal/installer/tar.go
+++ internal/installer/tar.go
@@ -80,6 +80,7 @@ func (i *TARPackage) Postinstall() error {
 func (i *TARPackage) Preinstall() error {
 	// simplify layout
 	cmds := [][]string{
+		{"rm", "-fr", "/elastic-agent"},
 		{"mv", fmt.Sprintf("/%s-%s-%s-%s", i.artifact, i.version, i.OS, i.arch), "/elastic-agent"},
 	}
 	for _, cmd := range cmds {
