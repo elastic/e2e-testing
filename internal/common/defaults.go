5c8fedc17fedddc3c0eba6af8c9c1e98deb29dc1
Manuel de la Peña <mdelapenya@gmail.com>
2021-06-28 10:20:59 +0200

    fix: use 7.13 alias

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -40,7 +40,7 @@ const FleetServerAgentServiceName = "fleet-server"
 var AgentStaleVersion = "7.13-SNAPSHOT"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = "7.13.0-SNAPSHOT"
+var BeatVersionBase = "7.13-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
