b74179bf3aa17d80561b8ac4e52853de41610fcf
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-08-03 15:48:07 -0400

    Use expand-archive pwsh for extracting
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

--- internal/installer/elasticagent_zip.go
+++ internal/installer/elasticagent_zip.go
@@ -124,7 +124,7 @@ func (i *elasticAgentZIPPackage) Preinstall(ctx context.Context) error {
 		return err
 	}
 
-	output, err := i.Exec(ctx, []string{"unzip", binaryPath})
+	output, err := i.Exec(ctx, []string{"powershell.exe", "Expand-Archive", "-LiteralPath", binaryPath})
 	if err != nil {
 		return err
 	}
