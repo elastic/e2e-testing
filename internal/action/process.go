8e9b085798c9e0227a841a49465aec1f5f744f42
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-09-07 13:54:04 -0400

    Move OS specific commands into actions (#1519)
    * Move OS specific commands into actions
    
    This adds support for what I am calling ServiceOperatorAction. We are moving
    specific executions into this namespace so that depending on the action we want
    to run it will do the right thing on the underlying OS.
    
    This is part 1 of the work to pave the way for supporting Windows with our first
    action on checking process state.
    
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>
    
    * use type assertion for action opts
    
    This lets us generalize the options passed to an action since each action will
    already have a known type to handle any additional options needed for the action
    to perform.
    
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>
    
    * Minor cleanups
    
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>
    (cherry picked from commit 4fce879df6d776caf98cd5452953c8c25f564d3e)

---
+++ internal/action/process.go
@@ -0,0 +1,242 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package action
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
+	"github.com/elastic/e2e-testing/internal/common"
+	"github.com/elastic/e2e-testing/internal/deploy"
+	"github.com/elastic/e2e-testing/internal/utils"
+	log "github.com/sirupsen/logrus"
+)
+
+// ProcessAction contains the necessary options to pass into process action
+type ProcessAction struct {
+	Process      string
+	DesiredState string
+	Occurrences  int
+	MaxTimeout   time.Duration
+}
+
+// actionWaitProcess implements operation for waiting on a process status
+type actionWaitProcess struct {
+	service deploy.ServiceRequest
+	deploy  deploy.Deployment
+	opts    ProcessAction
+}
+
+// AttachActionWaitProcess action to woit for a process status on *nix like systems
+func AttachActionWaitProcess(deploy deploy.Deployment, service deploy.ServiceRequest, actionOpts ProcessAction) deploy.ServiceOperation {
+	return &actionWaitProcess{
+		service: service,
+		deploy:  deploy,
+		opts:    actionOpts,
+	}
+}
+
+// Run executes the command
+func (a *actionWaitProcess) Run(ctx context.Context) (string, error) {
+	exp := utils.GetExponentialBackOff(a.opts.MaxTimeout)
+
+	mustBePresent := false
+	if a.opts.DesiredState == "started" {
+		mustBePresent = true
+	}
+	retryCount := 1
+
+	processStatus := func() error {
+		log.WithFields(log.Fields{
+			"desiredState": a.opts.DesiredState,
+			"occurrences":  a.opts.Occurrences,
+			"process":      a.opts.Process,
+		}).Trace("Checking process desired state on the container")
+
+		// pgrep -d: -d, --delimiter <string>  specify output delimiter
+		//i.e. "pgrep -d , metricbeat": 483,519
+		cmds := []string{"pgrep", "-d", ",", a.opts.Process}
+		output, err := a.deploy.ExecIn(ctx, common.FleetProfileServiceRequest, a.service, cmds)
+		if err != nil {
+			if !mustBePresent && a.opts.Occurrences == 0 {
+				log.WithFields(log.Fields{
+					"cmds":          cmds,
+					"desiredState":  a.opts.DesiredState,
+					"elapsedTime":   exp.GetElapsedTime(),
+					"error":         err,
+					"service":       a.service,
+					"mustBePresent": mustBePresent,
+					"occurrences":   a.opts.Occurrences,
+					"process":       a.opts.Process,
+					"retry":         retryCount,
+				}).Warn("Process is not present and number of occurences is 0")
+				return nil
+			}
+
+			log.WithFields(log.Fields{
+				"cmds":          cmds,
+				"desiredState":  a.opts.DesiredState,
+				"elapsedTime":   exp.GetElapsedTime(),
+				"error":         err,
+				"service":       a.service,
+				"mustBePresent": mustBePresent,
+				"occurrences":   a.opts.Occurrences,
+				"process":       a.opts.Process,
+				"retry":         retryCount,
+			}).Warn("Could not get number of processes in the container")
+
+			retryCount++
+
+			return err
+		}
+
+		// tokenize the pids to get each pid's state, adding them to an array if they match the desired state
+		// From Split docs:
+		// If output does not contain sep and sep is not empty, Split returns a
+		// slice of length 1 whose only element is s, that's why we first initialise to the empty array
+		pids := strings.Split(output, ",")
+		if len(pids) == 1 && pids[0] == "" {
+			pids = []string{}
+		}
+
+		log.WithFields(log.Fields{
+			"count":         len(pids),
+			"desiredState":  a.opts.DesiredState,
+			"mustBePresent": mustBePresent,
+			"pids":          pids,
+			"process":       a.opts.Process,
+		}).Tracef("Pids for process found")
+
+		desiredStatePids := []string{}
+
+		for _, pid := range pids {
+			pidStateCmds := []string{"ps", "-q", pid, "-o", "state", "--no-headers"}
+			pidState, err := a.deploy.ExecIn(ctx, common.FleetProfileServiceRequest, a.service, pidStateCmds)
+			if err != nil {
+				log.WithFields(log.Fields{
+					"cmds":          cmds,
+					"desiredState":  a.opts.DesiredState,
+					"elapsedTime":   exp.GetElapsedTime(),
+					"error":         err,
+					"service":       a.service,
+					"mustBePresent": mustBePresent,
+					"occurrences":   a.opts.Occurrences,
+					"pid":           pid,
+					"process":       a.opts.Process,
+					"retry":         retryCount,
+				}).Warn("Could not check pid status in the container")
+
+				retryCount++
+
+				return err
+			}
+
+			log.WithFields(log.Fields{
+				"desiredState":  a.opts.DesiredState,
+				"mustBePresent": mustBePresent,
+				"pid":           pid,
+				"pidState":      pidState,
+				"process":       a.opts.Process,
+			}).Tracef("Checking if process is in the S state")
+
+			// if the process must be present, then check for the S state
+			// From 'man ps':
+			// D    uninterruptible sleep (usually IO)
+			// R    running or runnable (on run queue)
+			// S    interruptible sleep (waiting for an event to complete)
+			// T    stopped by job control signal
+			// t    stopped by debugger during the tracing
+			// W    paging (not valid since the 2.6.xx kernel)
+			// X    dead (should never be seen)
+			// Z    defunct ("zombie") process, terminated but not reaped by its parent
+			if mustBePresent && pidState == "S" {
+				desiredStatePids = append(desiredStatePids, pid)
+			} else if !mustBePresent {
+				desiredStatePids = append(desiredStatePids, pid)
+			}
+		}
+
+		occurrencesMatched := (len(desiredStatePids) == a.opts.Occurrences)
+
+		// both true or both false
+		if mustBePresent == occurrencesMatched {
+			log.WithFields(log.Fields{
+				"desiredOccurrences": a.opts.Occurrences,
+				"desiredState":       a.opts.DesiredState,
+				"service":            a.service,
+				"mustBePresent":      mustBePresent,
+				"occurrences":        len(desiredStatePids),
+				"process":            a.opts.Process,
+			}).Infof("Process desired state checked")
+
+			return nil
+		}
+
+		if mustBePresent {
+			err = fmt.Errorf("%s process is not running in the container with the desired number of occurrences (%d) yet", a.opts.Process, a.opts.Occurrences)
+			log.WithFields(log.Fields{
+				"desiredOccurrences": a.opts.Occurrences,
+				"desiredState":       a.opts.DesiredState,
+				"elapsedTime":        exp.GetElapsedTime(),
+				"error":              err,
+				"service":            a.service,
+				"occurrences":        len(desiredStatePids),
+				"process":            a.opts.Process,
+				"retry":              retryCount,
+			}).Warn(err.Error())
+
+			retryCount++
+
+			return err
+		}
+
+		err = fmt.Errorf("%s process is still running in the container", a.opts.Process)
+		log.WithFields(log.Fields{
+			"desiredOccurrences": a.opts.Occurrences,
+			"elapsedTime":        exp.GetElapsedTime(),
+			"error":              err,
+			"service":            a.service,
+			"occurrences":        len(desiredStatePids),
+			"process":            a.opts.Process,
+			"state":              a.opts.DesiredState,
+			"retry":              retryCount,
+		}).Warn(err.Error())
+
+		retryCount++
+
+		return err
+	}
+
+	err := backoff.Retry(processStatus, exp)
+	if err != nil {
+		return "", err
+	}
+
+	return "", nil
+}
+
+// actionWaitProcessWin implements operation for waiting on a process on Windows
+type actionWaitProcessWin struct {
+	service deploy.ServiceRequest
+	deploy  deploy.Deployment
+	opts    ProcessAction
+}
+
+// AttachActionWaitProcessWin action to wait for process status on windows systems
+func AttachActionWaitProcessWin(deploy deploy.Deployment, service deploy.ServiceRequest, actionOpts ProcessAction) deploy.ServiceOperation {
+	return &actionWaitProcessWin{
+		service: service,
+		deploy:  deploy,
+		opts:    actionOpts,
+	}
+}
+
+// Run executes the command
+func (a *actionWaitProcessWin) Run(ctx context.Context) (string, error) {
+	return "", nil
+}
