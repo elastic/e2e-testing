c1df631cdfaa67d3b3fd0e8f3201ee90b543b2c4
Manuel de la Peña <mdelapenya@gmail.com>
2021-09-08 17:06:23 +0200

    feat: support elastic-package provider for Fleet test suite (#1502)
    * chore: go mod tidy
    
    * chore: add elastic-package as an external dependency
    
    * chore: pass context to logs
    
    * chore: add APM traces for pre-bootstrap
    
    * chore: pass context to remove method
    
    * chore: pass context to start/stop methods
    
    * chore: update references
    
    * chore: decouple common from deploy package
    
    * chore: simplify container name calculation
    
    We are going to read it from the Docker engine
    
    * chore: always try to remove the elastic-agent after scenarios
    
    * fix: go mod dependencies
    
    * chore: git clone integrations instead of Beats
    
    * Revert "chore: git clone integrations instead of Beats"
    
    This reverts commit db4b45ead099c87e1a5652903d35e4a65ef0dcc3.
    
    * chore: move sync-integrations to config package
    
    * chore: always sync integrations in metricbeat test suite
    
    For that reason we are removing the sync command, as it's useless
    
    * fix: order to Inspect a container matters
    
    * chore: always remove the elastic-agent container after scenario
    
    * chore: add comments about agent logs in the tear down phase
    
    * feat: add an elastic-package provider
    
    First, it will only support the Fleet test suite, exiting with an error
    if it's another test suite.
    
    Then, it will use elastic-package to provision the runtime dependencies
    of the Fleet test suite, running the stack withouth the elastic-agent
    service. This service will be provisioned using testcontainers directly,
    using the existing base images: centos/debian with systemd and the oficial
    Docker image for the elastic-agent
    
    * chore(ci): convert platforms into an object
    
    The identifier will be labels
    
    * chore: support defining service provider at the platform level
    
    * fix: pass object, not string
    
    * chore: use Provider from common package
    
    * chore: bump Go version to 1.16
    
    * fix: forgot formatter
    
    * chore: handle deletion of the container if it does not exist
    
    * chore: expand elastic-package coverage
    
    * chore: use docker provider on ARM
    
    * chore: fix comment
    
    * chore: rename go file
    
    * chore: use filepath instead of path
    
    * fix: use same directory but os-agnostic
    
    * chore: update paths to be os-agnostic in tests
    
    * chore: add unit tests
    
    * fix: remove unused import after conflicts
    
    * chore: remove scheduled pipelines for integrations
    
    * chore: remove metricbeat test suite from CI
    
    * chore: remove metricbeat code
    
    * fix: there are no unit tests in the e2e dir

---
+++ internal/deploy/elastic_package.go
@@ -0,0 +1,331 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package deploy
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/elastic/e2e-testing/internal/common"
+	"github.com/elastic/e2e-testing/internal/shell"
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+	"github.com/testcontainers/testcontainers-go"
+	tc "github.com/testcontainers/testcontainers-go"
+	"go.elastic.co/apm"
+)
+
+const elasticPackagePrefix = "elastic-package-stack"
+
+// elasticPackageBaseCommand represents the command to run 'elastic-package'. As it's declared as a dependency (see tools.go)
+// we will run it with 'go run github.com/elastic/elastic-package' instead of running the binary from GOPATH.
+var elasticPackageBaseCommand = []string{"run", "github.com/elastic/elastic-package"}
+
+// EPServiceManager manages lifecycle of a service using elastic-package tool
+type EPServiceManager struct {
+	Context context.Context
+}
+
+func newElasticPackage() Deployment {
+	return &EPServiceManager{
+		Context: context.Background(),
+	}
+}
+
+// Add adds services deployment: the first service in the list must be the profile in which to deploy the service
+func (ep *EPServiceManager) Add(ctx context.Context, profile ServiceRequest, services []ServiceRequest, env map[string]string) error {
+	version := common.BeatVersion
+
+	span, _ := apm.StartSpanOptions(ctx, "Adding elastic-agent to Elastic-Package deployment", "elastic-package.elastic-agent.add", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	span.Context.SetLabel("beatVersion", version)
+	span.Context.SetLabel("profile", profile)
+	span.Context.SetLabel("services", services)
+	defer span.End()
+
+	if profile.Name != "fleet" {
+		return fmt.Errorf("profile %s not supported in elastic-package provisioner. Services: %v", profile.Name, services)
+	}
+
+	for _, srv := range services {
+		if srv.Name != "elastic-agent" {
+			if srv.Name != common.ElasticAgentServiceName {
+				return fmt.Errorf("service %s not supported in elastic-package provisioner. Profile: %s", srv.Name, profile.Name)
+			}
+		}
+
+		_, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+			ContainerRequest: buildElasticAgentRequest(srv, env),
+			Started:          true,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Bootstrap sets up environment with docker compose
+func (ep *EPServiceManager) Bootstrap(ctx context.Context, profile ServiceRequest, env map[string]string, waitCB func() error) error {
+	services := "elasticsearch,fleet-server,kibana,package-registry"
+
+	version := common.StackVersion
+
+	args := append(elasticPackageBaseCommand, "stack", "up", "--daemon", "--verbose", "--version", version, "--services", services)
+
+	span, _ := apm.StartSpanOptions(ctx, "Bootstrapping Elastic Package deployment", "elastic-package.manifest.bootstrap", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	span.Context.SetLabel("args", args)
+	span.Context.SetLabel("profile", profile)
+	span.Context.SetLabel("services", services)
+	span.Context.SetLabel("stackVersion", version)
+	defer span.End()
+
+	if profile.Name != "fleet" {
+		return fmt.Errorf("profile %s not supported in elastic-package provisioner. Services: %v", profile.Name, services)
+	}
+
+	_, err := shell.Execute(ctx, ".", "go", args...)
+	return err
+}
+
+// AddFiles - add files to service
+func (ep *EPServiceManager) AddFiles(ctx context.Context, profile ServiceRequest, service ServiceRequest, files []string) error {
+	// TODO: profile is not used because we are using the docker client, not docker-compose, to reach the service
+	span, _ := apm.StartSpanOptions(ctx, "Adding files to Elastic-Package deployment", "elastic-package.files.add", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	span.Context.SetLabel("files", files)
+	span.Context.SetLabel("profile", profile)
+	span.Context.SetLabel("service", service)
+	defer span.End()
+
+	container, _ := ep.Inspect(ctx, service)
+	for _, file := range files {
+		isTar := true
+		fileExt := filepath.Ext(file)
+		if fileExt == ".rpm" || fileExt == ".deb" {
+			isTar = false
+		}
+		err := CopyFileToContainer(ctx, container.Name, file, "/", isTar)
+		if err != nil {
+			log.WithField("error", err).Fatal("Unable to copy file to service")
+		}
+	}
+	return nil
+}
+
+// Destroy teardown docker environment
+func (ep *EPServiceManager) Destroy(ctx context.Context, profile ServiceRequest) error {
+	span, _ := apm.StartSpanOptions(ctx, "Destroying Elastic-Package deployment", "elastic-package.manifest.destroy", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	span.Context.SetLabel("profile", profile)
+	defer span.End()
+
+	if profile.Name != "fleet" {
+		return fmt.Errorf("profile %s not supported in elastic-package provisioner", profile.Name)
+	}
+
+	_, err := shell.Execute(ctx, ".", "go", append(elasticPackageBaseCommand, "stack", "down", "--verbose")...)
+	return err
+}
+
+// ExecIn execute command in service
+func (ep *EPServiceManager) ExecIn(ctx context.Context, profile ServiceRequest, service ServiceRequest, cmd []string) (string, error) {
+	span, _ := apm.StartSpanOptions(ctx, "Executing command in Elastic-Package deployment", "elastic-package.manifest.execIn", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	span.Context.SetLabel("profile", profile)
+	span.Context.SetLabel("service", service)
+	span.Context.SetLabel("arguments", cmd)
+	defer span.End()
+
+	if profile.Name != "fleet" {
+		return "", fmt.Errorf("profile %s not supported in elastic-package provisioner. Service: %v", profile.Name, service)
+	}
+
+	inspect, _ := ep.Inspect(ctx, service)
+
+	args := []string{"exec", "-u", "root", "-i", inspect.Name}
+	args = append(args, cmd...)
+
+	output, err := shell.Execute(ctx, ".", "docker", args...)
+	if err != nil {
+		return "", err
+	}
+	return output, nil
+}
+
+// Inspect inspects a service
+func (ep *EPServiceManager) Inspect(ctx context.Context, service ServiceRequest) (*ServiceManifest, error) {
+	span, _ := apm.StartSpanOptions(ctx, "Inspecting Elastic Package deployment", "elastic-package.manifest.inspect", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	span.Context.SetLabel("service", service)
+	defer span.End()
+
+	inspect, err := InspectContainer(service)
+	if err != nil {
+		return &ServiceManifest{}, err
+	}
+
+	return &ServiceManifest{
+		ID:         inspect.ID,
+		Name:       strings.TrimPrefix(inspect.Name, "/"),
+		Connection: service.Name,
+		Alias:      inspect.NetworkSettings.Networks["elastic-package-stack_default"].Aliases[0],
+		Hostname:   inspect.Config.Hostname,
+		Platform:   inspect.Platform,
+	}, nil
+}
+
+// Logs print logs of service
+func (ep *EPServiceManager) Logs(ctx context.Context, service ServiceRequest) error {
+	span, _ := apm.StartSpanOptions(ctx, "Retrieving Elastic Package logs", "elastic-package.manifest.logs", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	span.Context.SetLabel("service", service)
+	defer span.End()
+
+	manifest, _ := ep.Inspect(context.Background(), service)
+	_, err := shell.Execute(ep.Context, ".", "docker", "logs", manifest.Name)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":   err,
+			"service": service.Name,
+		}).Error("Could not retrieve Elastic Agent logs")
+
+		return err
+	}
+	return nil
+}
+
+// PreBootstrap sets up environment with the elastic-package tool
+func (ep *EPServiceManager) PreBootstrap(ctx context.Context) error {
+	span, _ := apm.StartSpanOptions(ctx, "Pre-bootstrapping elastic-package deployment", "elastic-package.bootstrap.pre", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	defer span.End()
+
+	return nil
+}
+
+// Remove remove services from deployment
+func (ep *EPServiceManager) Remove(ctx context.Context, profile ServiceRequest, services []ServiceRequest, env map[string]string) error {
+	span, _ := apm.StartSpanOptions(ctx, "Removing services from Elastic Package deployment", "elastic-package.services.remove", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	span.Context.SetLabel("profile", profile)
+	span.Context.SetLabel("services", services)
+	defer span.End()
+
+	if profile.Name != "fleet" {
+		return fmt.Errorf("profile %s not supported in elastic-package provisioner. Services: %v", profile.Name, services)
+	}
+
+	for _, service := range services {
+		manifest, inspectErr := ep.Inspect(context.Background(), service)
+		if inspectErr != nil {
+			log.Warnf("Service %s could not be deleted: %v", service.Name, inspectErr)
+			continue
+		}
+
+		_, err := shell.Execute(ep.Context, ".", "docker", "rm", "-fv", manifest.Name)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Start a container
+func (ep *EPServiceManager) Start(ctx context.Context, service ServiceRequest) error {
+	span, _ := apm.StartSpanOptions(ctx, "Starting service from Elastic Package deployment", "elastic-package.service.start", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	span.Context.SetLabel("service", service)
+	defer span.End()
+
+	manifest, _ := ep.Inspect(context.Background(), service)
+	_, err := shell.Execute(ep.Context, ".", "docker", "start", manifest.Name)
+	return err
+}
+
+// Stop a container
+func (ep *EPServiceManager) Stop(ctx context.Context, service ServiceRequest) error {
+	span, _ := apm.StartSpanOptions(ctx, "Stopping service from Elastic Package deployment", "elastic-package.service.stop", apm.SpanOptions{
+		Parent: apm.SpanFromContext(ctx).TraceContext(),
+	})
+	span.Context.SetLabel("service", service)
+	defer span.End()
+
+	manifest, _ := ep.Inspect(context.Background(), service)
+	_, err := shell.Execute(ep.Context, ".", "docker", "stop", manifest.Name)
+	return err
+}
+
+func buildElasticAgentRequest(srv ServiceRequest, env map[string]string) tc.ContainerRequest {
+	privileged := false
+	var bindMounts map[string]string
+	var entrypoint []string
+	imageNamespace := fmt.Sprintf("elastic-agent%s", env["elasticAgentDockerImageSuffix"])
+	img := fmt.Sprintf("docker.elastic.co/%s/%s:%s", env["elasticAgentDockerNamespace"], imageNamespace, env["elasticAgentTag"])
+
+	if srv.Flavour == "centos" || srv.Flavour == "debian" {
+		imageNamespace = fmt.Sprintf("elastic-agent-%s", srv.Flavour)
+		// use observability's systemd base images to install the elastic-agent on them
+		img = "docker.elastic.co/observability-ci/" + srv.Flavour + "-systemd:latest"
+		entrypoint = []string{"/sbin/init"}
+		bindMounts = map[string]string{
+			"/sys/fs/cgroup": "/sys/fs/cgroup",
+		}
+		privileged = true
+	} else if srv.Flavour == "cloud" {
+		bindMounts = map[string]string{
+			env["apmVolume"]: "/apm-legacy",
+		}
+		env["FLEET_SERVER_ENABLE"] = "1"
+		env["FLEET_SERVER_INSECURE_HTTP"] = "1"
+		env["ELASTIC_AGENT_CLOUD"] = "1"
+		env["APM_SERVER_PATH"] = "/apm-legacy/apm-server/"
+		env["STATE_PATH"] = "/apm-legacy/elastic-agent/"
+		env["DATA_PATH"] = "/apm-legacy/data/"
+		env["LOGS_PATH"] = "/apm-legacy/logs/"
+		env["HOME_PATH"] = "/apm-legacy/"
+	} else if srv.Flavour == "" {
+		// Docker image for the agent
+		env["FLEET_SERVER_ENABLE"] = env["fleetServerMode"]
+		env["FLEET_SERVER_INSECURE_HTTP"] = env["fleetServerMode"]
+		env["FLEET_ENROLL"] = env["fleetEnroll"]
+		env["FLEET_ENROLLMENT_TOKEN"] = env["fleetEnrollmentToken"]
+		env["FLEET_INSECURE"] = env["fleetInsecure"]
+		env["FLEET_URL"] = env["fleetUrl"]
+	}
+
+	req := tc.ContainerRequest{
+		BindMounts: bindMounts,
+		Env:        env,
+		Image:      img,
+		Labels: map[string]string{
+			"name": srv.Name, //label is important to handle Inspect
+		},
+		Name:       fmt.Sprintf("%s_%s_%s_%d", elasticPackagePrefix, imageNamespace, uuid.New().String(), srv.Scale),
+		Privileged: privileged,
+		Networks:   []string{elasticPackagePrefix + "_default"},
+		SkipReaper: common.DeveloperMode, // skip reaping the container if developers required it using the "DEVELOPER_MODE=true" env var
+	}
+
+	if len(entrypoint) > 0 {
+		req.Entrypoint = entrypoint
+	}
+
+	return req
+}
