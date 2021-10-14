f5771d390ef83cf1cab1bb1a8e39e528050abaf3
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-10-14 16:40:18 -0400

    push more of infra to terraform
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

--- .ci/terraform/centos/terraform.tf
+++ .ci/terraform/centos/terraform.tf
@@ -40,12 +40,35 @@ resource "google_compute_instance" "default" {
       "sudo yum -y install rsync wget gcc",
       "wget https://dl.google.com/go/go1.16.3.linux-amd64.tar.gz",
       "sudo tar -C /usr/local -xf go1.16.3.linux-amd64.tar.gz",
-      "echo \"PATH=$PATH:/usr/local/go/bin\" | sudo tee -a /etc/profile",
+      "echo \"export PATH=$PATH:/usr/local/go/bin\" | sudo tee -a /etc/profile",
       "mkdir -p /home/${var.user}/e2e-testing",
     ]
   }
 
  provisioner "local-exec" {
-   command = "rsync -avz --exclude='.git/' -e \"ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${var.workspace}/e2essh\" ${var.workspace}/* ci@${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}:/home/${var.user}/e2e-testing/"
+   command = "rsync -avz --exclude='.git/' -e \"ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${var.privatekeypath}/e2essh\" ${var.base_dir}/* ci@${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}:/home/${var.user}/e2e-testing/"
+  }
+
+ provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.privatekeypath)}"
+      host = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+      agent = "false"
+    }
+
+    inline = [
+      "echo \"export GOARCH=${var.goarch}\" | sudo tee -a /etc/profile",
+      "echo \"export PROVIDER=${var.provider_type}\" | sudo tee -a /etc/profile",
+      "echo \"export LOG_LEVEL=${var.log_level}\" | sudo tee -a /etc/profile",
+      "echo \"export OP_LOG_LEVEL=${var.log_level}\" | sudo tee -a /etc/profile",
+      "echo \"export KIBANA_URL=${var.kibana_url}\" | sudo tee -a /etc/profile",
+      "echo \"export KIBANA_PASSWORD=${var.kibana_password}\" | sudo tee -a /etc/profile",
+      "echo \"export ELASTICSEARCH_URL=${var.elasticsearch_url}\" | sudo tee -a /etc/profile",
+      "echo \"export ELASTICSEARCH_PASSWORD=${var.elasticsearch_password}\" | sudo tee -a /etc/profile",
+      "echo \"export FLEET_URL=${var.fleet_url}\" | sudo tee -a /etc/profile",
+      "echo \"export SKIP_PULL=${var.skip_pull}\" | sudo tee -a /etc/profile",
+    ]
   }
 }
--- .ci/terraform/centos/variables.tf
+++ .ci/terraform/centos/variables.tf
@@ -17,3 +17,41 @@ variable "workspace" {
   type = string
   default = "/tmp"
 }
+
+variable "goarch" {
+  type = string
+  default = "amd64"
+}
+
+variable "provider_type" {
+  type = string
+  default = "remote"
+}
+
+variable "log_level" {
+  type = string
+  default = "TRACE"
+}
+
+variable "kibana_url" {
+  type = string
+}
+variable "kibana_password" {
+  type = string
+}
+variable "elasticsearch_url" {
+  type = string
+}
+variable "elasticsearch_password" {
+  type = string
+}
+variable "fleet_url" {
+  type = string
+}
+variable "skip_pull" {
+  type = string
+  default = "1"
+}
+variable "base_dir" {
+  type = string
+}
