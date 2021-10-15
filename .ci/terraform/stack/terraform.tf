0b31d02d154ec247e0c004c12ae24cac29fe9243
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-10-15 13:24:19 -0400

    fix execssh
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

--- .ci/terraform/stack/terraform.tf
+++ .ci/terraform/stack/terraform.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     ec = {
@@ -11,8 +12,12 @@ provider "ec" {
    endpoint = "https://staging.found.no/"
 }
 
+resource "random_id" "instance_id" {
+  byte_length = 8
+}
+
 resource "ec_deployment" "end-to-end" {
-  name                   = "end-to-end"
+  name                   = "end-to-end-${random_id.instance_id.hex}"
   region                 = "gcp-us-central1"
   version                = "7.15.1"
   deployment_template_id = "gcp-io-optimized-v2"
