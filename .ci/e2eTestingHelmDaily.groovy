427a13a886eeabcb4449d94e2b1a25b09df1427b
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-10-19 10:49:12 +0100

    chore: for testing purposes

--- .ci/e2eTestingHelmDaily.groovy
+++ .ci/e2eTestingHelmDaily.groovy
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-@Library('apm@current') _
+@Library('apm@feature/build-e2e-step') _
 
 pipeline {
   agent none
