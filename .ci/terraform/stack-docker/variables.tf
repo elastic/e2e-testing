d345631ecbf3d2ab1515aba7e135f84add0f6caa
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-10-19 07:54:33 -0400

    add vars to deploydockerstack
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

---
+++ .ci/terraform/stack-docker/variables.tf
@@ -0,0 +1,23 @@
+variable "privatekeypath" {
+    type = string
+    default= ".ci/terraform/e2essh"
+}
+
+variable "publickeypath" {
+    type = string
+    default= ".ci/terraform/e2essh.pub"
+}
+
+variable "user" {
+  type = string
+  default = "ci"
+}
+
+variable "workspace" {
+  type = string
+  default = "/tmp"
+}
+
+variable "basedir" {
+  type = string
+}
