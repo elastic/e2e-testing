1627ba0ee243597eea83a9fc7a7f0425bfd2a619
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-10-19 07:41:59 -0400

    terraform docker deploy until cloud is fixed
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

---
+++ .ci/terraform/stack-docker/terraform.tf
@@ -0,0 +1,66 @@
+provider "google" {
+  project = "elastic-observability"
+  region = "us-central1"
+}
+
+resource "random_id" "instance_id" {
+  byte_length = 8
+}
+
+resource "google_compute_instance" "default" {
+  name = "e2e-${random_id.instance_id.hex}"
+  machine_type = "e2-standard-4"
+  zone = "us-central1-c"
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-8"
+    }
+  }
+
+  metadata = {
+    ssh-keys = "${var.user}:${file(var.publickeypath)}"
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+ provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.privatekeypath)}"
+      host = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+      agent = "false"
+    }
+
+    inline = [
+      "sudo yum -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine || true",
+      "sudo yum -y install yum-utils",
+      "sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
+      "sudo yum -y install docker-ce docker-ce-cli containerd.io",
+      "sudo systemctl start docker",
+      "mkdir -p /home/${var.user}/e2e-testing",
+    ]
+  }
+
+ provisioner "local-exec" {
+   command = "cd ${var.workspace}/${var.base_dir} && rsync -avz --exclude='.git/' --include='.ci/' -e \"ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${var.privatekeypath}\" ./ ci@${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}:/home/${var.user}/e2e-testing"
+  }
+
+ provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.privatekeypath)}"
+      host = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+      agent = "false"
+    }
+
+   inline = [
+     "sudo docker-compose -f /home/${var.user}/e2e-testing/cli/config/compose/profiles/fleet/docker-compose.yml up -d",
+    ]
+ }
+}
