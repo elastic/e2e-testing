73d403d91f5935ede700fa2bcb4a3b4a12f0e339
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-11-10 09:03:19 -0500

    Fix background process matcher
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

--- internal/action/process.go
+++ internal/action/process.go
@@ -45,21 +45,14 @@ func AttachActionWaitProcess(deploy deploy.Deployment, service deploy.ServiceReq
 func (a *actionWaitProcess) Run(ctx context.Context) (string, error) {
 	exp := utils.GetExponentialBackOff(a.opts.MaxTimeout)
 
-	mustBePresent := false
+	pidState := "stopped"
 	if a.opts.DesiredState == "started" {
-		mustBePresent = true
+		pidState = "sleep"
 	}
 	retryCount := 1
 
 	processStatus := func() error {
 		processes, err := process.Processes()
-
-		log.WithFields(log.Fields{
-			"desiredState":  a.opts.DesiredState,
-			"occurrences":   a.opts.Occurrences,
-			"process":       a.opts.Process,
-			"mustBePresent": mustBePresent,
-		}).Trace("Checking process desired state on the container")
 
 		desiredStatePids := []int32{}
 
@@ -69,70 +62,43 @@ func (a *actionWaitProcess) Run(ctx context.Context) (string, error) {
 			status, _ := p.Status()
 			ppid, _ := p.Ppid()
 			cmd, _ := p.Cmdline()
-			isRunning, _ := p.IsRunning()
 
-			if strings.EqualFold(processName, a.opts.Process) {
+			if strings.EqualFold(processName, a.opts.Process) && strings.EqualFold(status[0], pidState) {
+				desiredStatePids = append(desiredStatePids, pid)
 				log.WithFields(log.Fields{
-					"name":      processName,
-					"pid":       pid,
-					"ppid":      ppid,
-					"cmd":       cmd,
-					"isRunning": isRunning,
-					"status":    status,
-				}).Trace("Checking Process")
-				if mustBePresent && strings.EqualFold(status[0], "sleep") {
-					desiredStatePids = append(desiredStatePids, pid)
-				} else if !mustBePresent && strings.EqualFold(status[0], "stopped") {
-					desiredStatePids = append(desiredStatePids, pid)
-				} else if !mustBePresent {
-					desiredStatePids = append(desiredStatePids, pid)
-				}
-
+					"name":               processName,
+					"pid":                pid,
+					"ppid":               ppid,
+					"cmd":                cmd,
+					"status":             status,
+					"desiredState":       a.opts.DesiredState,
+					"desiredOccurrences": a.opts.Occurrences,
+					"foundOccurrences":   len(desiredStatePids),
+				}).Debug("Checking Process desired state")
 			}
 		}
 
 		occurrencesMatched := (len(desiredStatePids) == a.opts.Occurrences)
 
 		// both true or both false
-		if mustBePresent == occurrencesMatched {
+		if occurrencesMatched {
 			log.WithFields(log.Fields{
 				"desiredOccurrences": a.opts.Occurrences,
+				"foundOccurrences":   len(desiredStatePids),
 				"desiredState":       a.opts.DesiredState,
 				"service":            a.service,
-				"mustBePresent":      mustBePresent,
-				"occurrences":        len(desiredStatePids),
 				"process":            a.opts.Process,
 			}).Infof("Process desired state found")
 			return nil
 		}
-
-		if mustBePresent {
-			err = fmt.Errorf("%s process is not running in the container with the desired number of occurrences (%d) yet", a.opts.Process, a.opts.Occurrences)
-			log.WithFields(log.Fields{
-				"desiredOccurrences": a.opts.Occurrences,
-				"desiredState":       a.opts.DesiredState,
-				"elapsedTime":        exp.GetElapsedTime(),
-				"error":              err,
-				"service":            a.service,
-				"occurrences":        len(desiredStatePids),
-				"process":            a.opts.Process,
-				"retry":              retryCount,
-			}).Warn(err.Error())
-
-			retryCount++
-
-			return err
-		}
-
-		err = fmt.Errorf("%s process is still running in the container", a.opts.Process)
+		err = fmt.Errorf("%s process is not in the desiredState the desired number of occurrences (%d) yet", a.opts.Process, a.opts.Occurrences)
 		log.WithFields(log.Fields{
 			"desiredOccurrences": a.opts.Occurrences,
+			"foundOccurrences":   len(desiredStatePids),
+			"desiredState":       a.opts.DesiredState,
 			"elapsedTime":        exp.GetElapsedTime(),
-			"error":              err,
 			"service":            a.service,
-			"occurrences":        len(desiredStatePids),
 			"process":            a.opts.Process,
-			"state":              a.opts.DesiredState,
 			"retry":              retryCount,
 		}).Warn(err.Error())
 
