5b639dce6b0987895a9ace112593dbeabf0ecbe5
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-11-16 16:08:13 -0500

    increase timeout wait in restart of elastic-agent
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

--- e2e/_suites/fleet/fleet.go
+++ e2e/_suites/fleet/fleet.go
@@ -487,7 +487,7 @@ func (fts *FleetTestSuite) processStateChangedOnTheHost(process string, state st
 			return err
 		}
 
-		utils.Sleep(time.Duration(utils.TimeoutFactor) * 20 * time.Second)
+		utils.Sleep(time.Duration(utils.TimeoutFactor) * 30 * time.Second)
 
 		err = agentInstaller.Start(fts.currentContext)
 		if err != nil {
