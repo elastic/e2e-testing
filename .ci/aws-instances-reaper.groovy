36067d3d9498d858d8e49a37cd143aac4af10d1d
Manuel de la Peña <mdelapenya@gmail.com>
2022-01-13 16:36:12 +0100

    chore: rename tag used on AWS reaper

--- .ci/aws-instances-reaper.groovy
+++ .ci/aws-instances-reaper.groovy
@@ -12,7 +12,8 @@ pipeline {
     PIPELINE_LOG_LEVEL = 'INFO'
     JOB_GIT_CREDENTIALS = "f6c7695a-671e-4f4f-a331-acdce44ff9ba"
     AWS_PROVISIONER_SECRET = 'secret/observability-team/ci/elastic-observability-aws-account-auth'
-    AWS_EC2_INSTANCES_TAG= 'e2e-testing-vm'
+    AWS_EC2_INSTANCES_TAG_NAME= 'ReaperMark'
+    AWS_EC2_INSTANCES_TAG_VALUE= 'e2e-testing-vm'
   }
   options {
     timeout(time: 1, unit: 'HOURS')
@@ -47,7 +48,7 @@ pipeline {
         deleteDir()
         unstash 'source'
         withAWSEnv(secret: "${env.AWS_PROVISIONER_SECRET}", forceInstallation: true) {
-          sh("aws ec2 terminate-instances --instance-ids `aws ec2 describe-instances --filters Name=tag:Kind,Values=${env.AWS_EC2_INSTANCES_TAG} --query Reservations[].Instances[].InstanceId --output text`")
+          sh("aws ec2 terminate-instances --instance-ids `aws ec2 describe-instances --filters Name=tag:${env.AWS_EC2_INSTANCES_TAG_NAME},Values=${env.AWS_EC2_INSTANCES_TAG_VALUE} --query Reservations[].Instances[].InstanceId --output text`")
         }
       }
     }
