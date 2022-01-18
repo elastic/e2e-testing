1429082cc54b35e75eddb3ee1cad6032eab5b091
Manuel de la Peña <mdelapenya@gmail.com>
2022-01-18 17:48:18 +0100

    chore: bump 7.16 version to use snapshots (#2017)
    * chore: bump 7.16 version to use snapshots
    
    This will allow the automated bump to send PRs with more accurate versions
    
    * bump stack version 7.16.3-26544d96

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -36,7 +36,7 @@ const FleetServerAgentServiceName = "fleet-server"
 var AgentStaleVersion = "7.15-SNAPSHOT"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = "7.16-SNAPSHOT"
+var BeatVersionBase = "7.16.3-26544d96-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
