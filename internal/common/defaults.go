0a4bdf5950713c88e3d66d394be64240cb814580
Manuel de la Peña <mdelapenya@gmail.com>
2022-01-19 18:19:41 +0100

    chore: bump more files

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -36,7 +36,7 @@ const FleetServerAgentServiceName = "fleet-server"
 var AgentStaleVersion = "7.16-SNAPSHOT"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = "7.17-SNAPSHOT"
+var BeatVersionBase = "7.17.0-5ce40764-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
