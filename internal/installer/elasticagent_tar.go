a6fd025847353698fa91deebc5afa8d619ae72c4
Manuel de la Peña <mdelapenya@gmail.com>
2022-02-28 19:08:57 +0100

    feat: support downloading project artifacts for the new bucket layout (#2172)
    * feat: support for the new bucket structure
    
    * feat: calculate bucket URLs using resolvers
    
    * chore: support for legacy Beats
    
    Old PRs needs to be tested and the binaries could not exist in the new layout
    
    * chore: support looking up the bucket in up-to 3 locations
    
    1. new project layout
    2. new beats layout
    3. legacy beats layout
    
    * chore: simplify boolean logic
    
    * fix: pass worker environment for versions to the VMs
    
    * fix: predefine kibana version
    
    * fix: remove extra char
    
    * fix: remove extra char
    
    * chore: log resolvers
    
    * chore: use new project method when fetching artifacts
    
    * fix: pass stack version to stack deployment
    
    * fix: use artifact to check for ubi8 variant
    
    * chore: improve logs in resolvers
    
    * fix: apply variants to project resolver
    
    Ubi8 is a valid variant for elastic-agent
    
    * chore: use Stack version for the dependant beats
    
    In the running-on-top-of-beats feature file, the code installs some Beats
    and we do not want to use the given SHA, because elastic-agent now lives
    in a separate repo, therefore they do not share commit SHAs
    
    * fix: use fleet-ci bucket for elastic-agent and fleet-server
    
    * chore: look up both CI buckets
    
    * chore: make useCISnapshots configurable on downloads
    
    * chore: background processes should not use CI snapshots
    
    * feat: separate elastic-agent version from beats version
    
    * chore: extract useCISnapshots to a function
    
    * chore: use Github_Check_Repo to identify where the Git commit lives
    
    * fix: k8s-autodiscover lives in both worlds
    
    * fix: missing package
    
    * fix: use snapshot version in k8s-autodiscover
    
    (cherry picked from commit 044dedf4543c83636762b3e23a9d58d389fd18bd)
    
    # Conflicts:
    #	.ci/Jenkinsfile

--- internal/installer/elasticagent_tar.go
+++ internal/installer/elasticagent_tar.go
@@ -108,8 +108,8 @@ func (i *elasticAgentTARPackage) Postinstall(ctx context.Context) error {
 
 // Preinstall executes operations before installing a TAR package
 func (i *elasticAgentTARPackage) Preinstall(ctx context.Context) error {
-	installArtifactFn := func(ctx context.Context, artifact string) error {
-		span, _ := apm.StartSpanOptions(ctx, "Pre-install operations for the Elastic Agent", "elastic-agent.tar.pre-install", apm.SpanOptions{
+	installArtifactFn := func(ctx context.Context, artifact string, version string, useCISnapshots bool) error {
+		span, _ := apm.StartSpanOptions(ctx, "Pre-install "+artifact, artifact+".tar.pre-install", apm.SpanOptions{
 			Parent: apm.SpanFromContext(ctx).TraceContext(),
 		})
 		defer span.End()
@@ -129,16 +129,16 @@ func (i *elasticAgentTARPackage) Preinstall(ctx context.Context) error {
 			}
 			log.Trace("Cleared previously downloaded artifacts")
 		}
-		_, binaryPath, err := downloads.FetchElasticArtifact(ctx, artifact, common.BeatVersion, runningOS, arch, extension, false, true)
+		_, binaryPath, err := downloads.FetchElasticArtifactForSnapshots(ctx, useCISnapshots, artifact, version, runningOS, arch, extension, false, true)
 		if err != nil {
 			log.WithFields(log.Fields{
 				"artifact":  artifact,
-				"version":   common.BeatVersion,
+				"version":   version,
 				"os":        runningOS,
 				"arch":      arch,
 				"extension": extension,
 				"error":     err,
-			}).Error("Could not download the binary for the agent")
+			}).Error("Could not download the binary")
 			return err
 		}
 
@@ -147,7 +147,7 @@ func (i *elasticAgentTARPackage) Preinstall(ctx context.Context) error {
 			return err
 		}
 
-		output, _ := i.Exec(ctx, []string{"mv", fmt.Sprintf("%s-%s-%s-%s", artifact, downloads.GetSnapshotVersion(common.BeatVersion), runningOS, arch), artifact})
+		output, _ := i.Exec(ctx, []string{"mv", fmt.Sprintf("%s-%s-%s-%s", artifact, downloads.GetSnapshotVersion(version), runningOS, arch), artifact})
 		log.WithFields(log.Fields{
 			"output":   output,
 			"artifact": artifact,
@@ -158,14 +158,14 @@ func (i *elasticAgentTARPackage) Preinstall(ctx context.Context) error {
 	for _, bp := range i.service.BackgroundProcesses {
 		if strings.EqualFold(bp, "filebeat") || strings.EqualFold(bp, "metricbeat") {
 			// pre-install the dependant binary first
-			err := installArtifactFn(ctx, bp)
+			err := installArtifactFn(ctx, bp, common.BeatVersion, downloads.UseBeatsCISnapshots())
 			if err != nil {
 				return err
 			}
 		}
 	}
 
-	return installArtifactFn(ctx, "elastic-agent")
+	return installArtifactFn(ctx, "elastic-agent", common.ElasticAgentVersion, downloads.UseElasticAgentCISnapshots())
 
 }
 
