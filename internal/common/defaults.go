a3d0592c53be081340838fec8e8c862bc40d8e65
Manuel de la Peña <mdelapenya@gmail.com>
2022-03-02 11:16:09 +0100

    fix: bump stale agent (#2198)

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -33,7 +33,7 @@ const FleetServerAgentServiceName = "fleet-server"
 
 // AgentStaleVersion is the version of the agent to use as a base during upgrade
 // It can be overriden by ELASTIC_AGENT_STALE_VERSION env var. Using latest GA as a default.
-var AgentStaleVersion = "7.16-SNAPSHOT"
+var AgentStaleVersion = "7.17.1-SNAPSHOT"
 
 // BeatVersionBase is the base version of the Beat to use
 var BeatVersionBase = "7.17.2-3e73f87b-SNAPSHOT"
