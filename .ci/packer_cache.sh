3f6dd5ba0616ce2744d27aeb70523335d9e7d39f
Manuel de la Peña <mdelapenya@gmail.com>
2022-03-21 16:30:10 +0100

    chore: bump Godog to v0.12.4 (#2245)
    * chore: bump godog to v.0.12.4
    
    * chore: update BeforeScenario/AfterScenario to Before/After
    
    * chore: update BeforeStep/AfterStep to StepContext().Before/After
    
    * chore: log before/after steps
    
    * chore: consistent variable name
    
    * chore: log step status after execution
    
    * chore: support multiple formatters
    
    * chore: store cucumber reports in Jenkins UI
    
    * chore: add go-license-detector as dependency
    
    * chore: notice
    
    * chore: store JSON reports as an artifact
    
    * fix: remove cucumber until the plugin is installed in the Jenkins instance
    
    * chore: update scenario template
    
    * fix: wrong log.Debugf signature
    
    * chore: generate cucumber reports without the plugin
    
    See https://github.com/elastic/e2e-testing/issues/120
    
    * chore: enrich cucumber reports metadata
    
    * fix: dockerLogin before pulling the new image
    
    * chore: update packer-cache images
    
    * fix: run under the right dir
    
    * fix: right path for cucumber-gen script
    
    * chore: simplify files variable
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>
    
    * chore: apply suggestions from code review
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>
    
    * chore: remove BASE_DIR
    
    * chore: update templates to godog v0.12.4
    
    * fix: update stack version in template
    
    * chore: update ES template
    
    * fix: use output dir from the nodeIP
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>
    (cherry picked from commit fb6385e1732e3f1399da00d40e0504c4bfe74159)
    
    # Conflicts:
    #	.ci/packer_cache.sh

---
+++ .ci/packer_cache.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1091
+source /usr/local/bin/bash_standard_lib.sh
+
+readonly GO_VERSION=$(cat .go-version)
+
+DOCKER_IMAGES="docker.elastic.co/observability-ci/cucumber-html-reporter:latest
+docker.elastic.co/observability-ci/picklesdoc:2.20.1
+golang:${GO_VERSION}-stretch
+"
+
+if [ -x "$(command -v docker)" ]; then
+  for di in ${DOCKER_IMAGES}
+  do
+  (retry 2 docker pull "${di}") || echo "Error pulling ${di} Docker image, we continue"
+  done
+fi
