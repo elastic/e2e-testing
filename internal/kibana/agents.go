5c46f559610d0c73c0a4884f3f886f1da144056f
Manuel de la Peña <mdelapenya@gmail.com>
2022-04-12 08:51:40 +0200

    fix: remove force parameter from payload
    See https://github.com/elastic/kibana/blame/main/x-pack/plugins/fleet/common/openapi/components/schemas/upgrade_agent.yaml

--- internal/kibana/agents.go
+++ internal/kibana/agents.go
@@ -276,7 +276,7 @@ func (c *Client) UpgradeAgent(ctx context.Context, hostname string, version stri
 		return err
 	}
 
-	reqBody := `{"version":"` + version + `", "force": true}`
+	reqBody := `{"version":"` + version + `"}`
 	statusCode, respBody, err := c.post(ctx, fmt.Sprintf("%s/agents/%s/upgrade", FleetAPI, agentID), []byte(reqBody))
 	if statusCode != 200 {
 		log.WithFields(log.Fields{
