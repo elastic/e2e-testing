108881672271ae1377704a3f9c02c1fa338b63f7
apmmachine <infra-root-apmmachine@elastic.co>
2022-04-29 05:16:37 +0000

    bump stack version 7.17.4-40d3b33d

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -36,7 +36,7 @@ const FleetServerAgentServiceName = "fleet-server"
 var AgentStaleVersion = "7.17.2-SNAPSHOT"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = "7.17.4-a30db1f9-SNAPSHOT"
+var BeatVersionBase = "7.17.4-40d3b33d-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
