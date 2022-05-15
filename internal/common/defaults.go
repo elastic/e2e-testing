eaff720991e73f87e7198ba7b4a11b6c50633de4
Manuel de la Peña <mdelapenya@gmail.com>
2022-05-15 10:46:41 +0200

    fix: install stale agent properly (#2354)
    * chore: add logs
    
    * chore: add missing dependency
    
    * chore: rename scenario for simplification
    
    * fix: download the stale agent version properly
    
    * revert: for testing purposes
    
    * fix: wrong regex
    
    * chore: move upgrade agent to its own feature file
    
    * revert: remove nightly for testing purpose
    
    * chore: add logs while waiting for upgrade agent
    
    * chore: remove duplication
    
    * fix: half the wait timeout for upgrading the agent
    
    It's 7 minutes on CI
    
    * chore: adjust logs
    
    * fix: do not restore the original version of the stale agent
    
    * fix: rename variable to match Go lint
    
    * fix: remove force parameter from payload
    
    See https://github.com/elastic/kibana/blame/main/x-pack/plugins/fleet/common/openapi/components/schemas/upgrade_agent.yaml
    
    * chore: include upgrades from latest to latest
    
    * chore: use upgrade command from the agent
    
    Instead of using kibana's APIs, we are goint to try the upgrade command
    
    * chore: extract artifacts URL resolver to a type
    
    * chore: add a resolver for downloads
    
    * chore: discover download URL from multiple resolvers
    
    * chore: support for HEAD requests
    
    * chore: wait for the HEAD request when checking downloads
    
    * chore: do not wait if 404 was returned
    
    * fix: use proper namespace for elastic-agent
    
    * chore: abstract upgrade command
    
    * chore: abstract elastic-agent package even more
    
    * chore: remove commit from the target version to upgrade
    
    * chore: pass package installer metadata
    
    * chore: download upgrade-to version before the upgrade
    
    * chore: support passing HTTP headers to kibana
    
    * chore: pass version header when upgrading an agent
    
    * chore: bring back upgrades from Fleet APIs
    
    * fix: print response body as a string
    
    * chore: print headers
    
    * chore: do not validate stale version alias
    
    it will be handled later during the download
    
    * chore: get the hostname only once
    
    * fix: pass the headers
    
    * fix: print out error if artifact is not found
    
    * fix: resolve stale agent version for aliases
    
    * fix: detect if version is an alias better
    
    * fix: remove hashed snapshot from the upgrade header
    
    * chore: test upgrades from 8.2 and above
    
    * fix: wait for the agent to be in the online status before performing the upgrade
    
    * chore: support checking for aliases in the downloads package
    
    * chore: resolve alias for installed versions
    
    * chore: rename method
    
    * feat: add a method to check agent status from the /agents API
    
    * fix: do not use hased versions in the upgrade version
    
    * chore: add more upgrade tests
    
    * chore: remove patch versions for 8.1
    
    * chore: test snapshots for 8.1 and 8.2
    
    * chore: do not pass kbn-version header while upgrading
    
    * chore: move upgrade scenarios to the nightly build
    
    (cherry picked from commit 724340dd99bb4a7a8dfa56e0e730958fabffafeb)
    
    # Conflicts:
    #	.ci/Jenkinsfile

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -5,8 +5,6 @@
 package common
 
 import (
-	"regexp"
-
 	"github.com/elastic/e2e-testing/internal/shell"
 	"github.com/elastic/e2e-testing/pkg/downloads"
 	log "github.com/sirupsen/logrus"
@@ -32,10 +30,6 @@ const FleetProfileName = "fleet"
 
 // FleetServerAgentServiceName the name of the service for the Elastic Agent
 const FleetServerAgentServiceName = "fleet-server"
-
-// AgentStaleVersion is the version of the agent to use as a base during upgrade
-// It can be overriden by ELASTIC_AGENT_STALE_VERSION env var. Using latest GA as a default.
-var AgentStaleVersion = "7.17-SNAPSHOT"
 
 // BeatVersionBase is the base version of the Beat to use
 var BeatVersionBase = "8.2.1-91d7a53f-SNAPSHOT"
@@ -66,10 +60,6 @@ var Provider = "docker"
 // It can be overriden by STACK_VERSION env var
 var StackVersion = BeatVersionBase
 
-// The compiled version of the regex created at init() is cached here so it
-// only needs to be created once.
-var versionRegex *regexp.Regexp
-
 func init() {
 	DeveloperMode = shell.GetEnvBool("DEVELOPER_MODE")
 	if DeveloperMode {
@@ -85,8 +75,6 @@ func init() {
 			"apm-environment": shell.GetEnv("ELASTIC_APM_ENVIRONMENT", "local"),
 		}).Info("Current execution will be instrumented 🛠")
 	}
-
-	versionRegex = regexp.MustCompile(`^([0-9]+)(\.[0-9]+)(-SNAPSHOT)?$`)
 }
 
 // InitVersions initialise default versions. We do not want to do it in the init phase
@@ -106,8 +94,7 @@ func InitVersions() {
 
 	// check if version is an alias. For compatibility versions let's
 	// support aliases in the format major.minor
-	m := versionRegex.FindStringSubmatch(BeatVersion)
-	if m != nil {
+	if downloads.IsAlias(BeatVersion) {
 		v, err = downloads.GetElasticArtifactVersion(BeatVersion)
 		if err != nil {
 			log.WithFields(log.Fields{
