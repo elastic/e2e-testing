f828814d35c82de2e5d7078cb557f7bcd807c660
Manuel de la Peña <mdelapenya@gmail.com>
2022-05-23 17:49:02 +0200

    fix: pass the location of the install dependencies script

--- .ci/scripts/install-test-dependencies.sh
+++ .ci/scripts/install-test-dependencies.sh
@@ -12,19 +12,20 @@ set -euxo pipefail
 #   - SUITE - that's the name of the test suite to install the dependencies for.
 #
 
-SUITE=${1:?SUITE is not set}
+WORKSPACE=${1:?WORKSPACE is not set}
+SUITE=${2:?SUITE is not set}
 
 # execute specific test dependencies if it exists
-if [ -f "$(pwd)/.ci/scripts/install-${SUITE}-test-dependencies.sh" ]
+if [ -f "${WORKSPACE}/.ci/scripts/install-${SUITE}-test-dependencies.sh" ]
 then
     ## Install the required dependencies with some retry
     CI_UTILS=/usr/local/bin/bash_standard_lib.sh
     if [ -e "${CI_UTILS}" ] ; then
         # shellcheck disable=SC1090
         source "${CI_UTILS}"
-        retry 3 source .ci/scripts/install-${SUITE}-test-dependencies.sh
+        retry 3 source ${WORKSPACE}/.ci/scripts/install-${SUITE}-test-dependencies.sh
     else
-        source .ci/scripts/install-${SUITE}-test-dependencies.sh
+        source ${WORKSPACE}/.ci/scripts/install-${SUITE}-test-dependencies.sh
     fi
 else
     echo "Not installing test dependencies for ${SUITE}"
