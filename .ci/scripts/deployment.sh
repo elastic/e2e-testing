4910b20b9410634e54885ee3c782773eea1a759b
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-06-14 15:46:53 +0100

    set config path

--- .ci/scripts/deployment.sh
+++ .ci/scripts/deployment.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-if [ -d .obs ] ; then
-    rm -rf .obs
+FOLDER=.obs
+if [ -d $FOLDER ] ; then
+    rm -rf $FOLDER
 fi
 git clone \
     --branch feature/remove-docker \
-    git@github.com:elastic/observability-test-environments.git .obs
+    git@github.com:elastic/observability-test-environments.git $FOLDER
 
-cd .obs
+cd $FOLDER
 
-export CLUSTER_CONFIG_FILE=tests/environments/elastic_cloud_tf_gcp.yml
+CONFIG_FILE=$SCRIPT_DIR/$FOLDER/tests/environments/elastic_cloud_tf_gcp.yml
 
 if [ $1 == "destroy" ] ; then
-    make -C ansible destroy-cluster
+    CLUSTER_CONFIG_FILE=$CONFIG_FILE make -C ansible destroy-cluster
 else
-    make -C ansible create-cluster
+    CLUSTER_CONFIG_FILE=$CONFIG_FILE make -C ansible create-cluster
 fi
