19a49b73cc245f24b18b7f8c9278f3eaa43edbcc
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-06-14 15:06:14 +0100

    use the same cloud deployment as the one in the apm-its
    https://github.com/elastic/apm-integration-testing/blob/71ae017baab37b177a2598ffb1a0c0aac64caeda/.ci/integrationTestEC.groovy\#L150

--- .ci/scripts/deployment.sh
+++ .ci/scripts/deployment.sh
@@ -11,10 +11,10 @@ git clone \
 
 cd .obs
 
+export CLUSTER_CONFIG_FILE=tests/environments/elastic_cloud_tf_gcp.yml
+
 if [ $1 == "destroy" ] ; then
-    CLUSTER_CONFIG_FILE=$SCRIPT_DIR/../elastic_cloud.yml \
     make -C ansible destroy-cluster
 else
-    CLUSTER_CONFIG_FILE=$SCRIPT_DIR/../elastic_cloud.yml \
     make -C ansible create-cluster
 fi
