123fa5ecfd236b4c9a018834b91dc937f9865722
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-06-15 13:29:42 +0100

    deployment: host definition

--- .ci/scripts/deployment.sh
+++ .ci/scripts/deployment.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 FOLDER=.obs
 if [ -d $FOLDER ] ; then
     rm -rf $FOLDER
@@ -10,15 +12,14 @@ git clone \
 
 cd $FOLDER
 
-CONFIG_FILE="$(pwd)"/tests/environments/elastic_cloud_tf_gcp.yml
+CONFIG_FILE=$SCRIPT_DIR/../elastic_cloud_tf_gcp.yml
 
-if [ ! -e $CONFIG_FILE ] ; then
+if [ ! -e "$CONFIG_FILE" ] ; then
   echo "$CONFIG_FILE does not exist"
-  ls -l .
   exit 1
 fi
 
-if [ $1 == "destroy" ] ; then
+if [ "$1" == "destroy" ] ; then
     CLUSTER_CONFIG_FILE=$CONFIG_FILE make -C ansible destroy-cluster
 else
     CLUSTER_CONFIG_FILE=$CONFIG_FILE make -C ansible create-cluster
