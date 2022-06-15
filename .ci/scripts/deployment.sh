07ef81fba3e57db0cd8fbcaa2a9e1e5976eaff5a
Victor Martinez <victormartinezrubio@gmail.com>
2022-06-15 12:23:22 +0100

    Apply suggestions from code review

--- .ci/scripts/deployment.sh
+++ .ci/scripts/deployment.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 FOLDER=.obs
 if [ -d $FOLDER ] ; then
     rm -rf $FOLDER
@@ -11,7 +10,13 @@ git clone \
 
 cd $FOLDER
 
-CONFIG_FILE=$SCRIPT_DIR/$FOLDER/tests/environments/elastic_cloud_tf_gcp.yml
+CONFIG_FILE="$(pwd)"/tests/environments/elastic_cloud_tf_gcp.yml
+
+if [ ! -e $CONFIG_FILE ] ; then
+  echo "$CONFIG_FILE does not exist"
+  ls -l .
+  exit 1
+fi
 
 if [ $1 == "destroy" ] ; then
     CLUSTER_CONFIG_FILE=$CONFIG_FILE make -C ansible destroy-cluster
