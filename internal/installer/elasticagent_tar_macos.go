8c9594251e77328ad297660af5f660644100b42e
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-06-16 11:06:40 +0100

    e2e: use relative path
    otherwise it fails in the ephemeral workers
    
    relative path was added in https://github.com/elastic/e2e-testing/issues/1740

--- internal/installer/elasticagent_tar_macos.go
+++ internal/installer/elasticagent_tar_macos.go
@@ -56,7 +56,7 @@ func (i *elasticAgentTARDarwinPackage) AddFiles(ctx context.Context, files []str
 func (i *elasticAgentTARDarwinPackage) Inspect() (deploy.ServiceOperatorManifest, error) {
 	return deploy.ServiceOperatorManifest{
 		WorkDir:    i.metadata.AgentPath,
-		CommitFile: "/elastic-agent/.elastic-agent.active.commit",
+		CommitFile: "elastic-agent/.elastic-agent.active.commit",
 	}, nil
 }
 
@@ -81,7 +81,7 @@ func (i *elasticAgentTARDarwinPackage) Exec(ctx context.Context, args []string) 
 
 // Enroll will enroll the agent into fleet
 func (i *elasticAgentTARDarwinPackage) Enroll(ctx context.Context, token string, extraFlags string) error {
-	cmds := []string{"/elastic-agent/elastic-agent", "install"}
+	cmds := []string{"./elastic-agent/elastic-agent", "install"}
 	span, _ := apm.StartSpanOptions(ctx, "Enrolling Elastic Agent with token", "elastic-agent.tar.enroll", apm.SpanOptions{
 		Parent: apm.SpanFromContext(ctx).TraceContext(),
 	})
