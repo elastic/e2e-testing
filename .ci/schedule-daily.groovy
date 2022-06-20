2f6d462f49d5414a64882f0247de254229e067ae
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-06-20 12:11:25 +0100

    remove for now

--- .ci/schedule-daily.groovy
+++ .ci/schedule-daily.groovy
@@ -21,7 +21,6 @@ pipeline {
     stage('Nighly e2e builds') {
       steps {
         runBuilds(quietPeriodFactor: 100, branches: ['main', '8.<minor>', '8.<next-patch>', '8.<next-minor>', '8.<minor-1>', '7.<minor>'])
-        // runBuildsForMacOS(branches: ['main', '8.<minor>'])
       }
     }
   }
@@ -42,15 +41,5 @@ def runBuilds(Map args = [:]) {
     build(quietPeriod: quietPeriod, job: "e2e-tests/e2e-testing-k8s-autodiscovery-daily-mbp/${branch}", wait: false, propagate: false)
     // Increate the quiet period for the next iteration
     quietPeriod += args.quietPeriodFactor
-  }
-}
-
-def runBuildsForMacOS(Map args = [:]) {
-  def branches = getBranchesFromAliases(aliases: args.branches)
-  def quietPeriod = 0
-  branches.each { branch ->
-    if (isBranchUnifiedReleaseAvailable(branch)) {
-      build(quietPeriod: quietPeriod, job: "e2e-tests/e2e-testing-macos-mbp/${branch}", wait: false, propagate: false)
-    }
   }
 }
