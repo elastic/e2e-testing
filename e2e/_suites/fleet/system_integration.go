e2b562866dba8256c5b3815a49b42d7548eb9268
Manuel de la Peña <mdelapenya@gmail.com>
2022-07-27 13:12:52 +0200

    chore: decouple responsibilities for fleet test suite code (#2850)
    * chore: move endpoint integration methods to a file
    
    * chore: move system integration methods to a file
    
    * chore: move stand-alone methods to its file
    
    * chore: move process-related methods to its file
    
    * chore: extract process check to its own internal package
    
    * chore: move top-level struct to remove file
    
    * fixup processes
    
    * chore: remove ingest_manager struct
    
    It causes confusion adding a new layer of indirection
    
    * chore: centralise adding steps to godog's scenario context
    
    * chore: rename main test file
    
    * chore: move before/after methods to the test file
    
    * chore: before/after methods are not from the struct
    
    * chore: extract upgrade steps to its file
    
    * chore: extract preconfigured_policy steps to its file
    
    * chore: extract integrations methods to a file
    
    * chore: simplify method for managing integrations
    
    * chore: extract more integrations methods
    
    * chore: extract more upgrade methods
    
    * chore: update pre-commit hooks
    
    * chore: extract api keys methods to its own file
    
    * chore: extract methods for agent enroll to its own file
    
    * chore: extract methods for agent deployment to its own file
    
    * chore: extract methods for agent checks to its own file
    
    * chore: extract methods for metrics/inputs to integrations files
    
    * fix: do not wait for kibana when the token is revoked
    
    Given https://github.com/elastic/kibana/issues/105078 is fixed, no need to wait for it anymore
    
    * chore: move struct and methods to test file
    
    * Revert "fix: do not wait for kibana when the token is revoked"
    
    This reverts commit c147114d2c866c8418bbd5b89fe2d01343698377.
    
    * chore: extract constants to their own files
    
    * Revert "chore: move struct and methods to test file"
    
    This reverts commit 2cf733d614df37d725b5252c73c67c2f8098bcc4.
    
    * chore: remove unused methods
    
    * chore: move search agent data to agent checks file
    
    * chore: move bootstrapFleet method to the test file
    
    * chore: rename initialiser methods
    
    * fix: rename test suite at Godog apis
    
    (cherry picked from commit fb0e134b7e6868a28e11846568b2da5180556fb5)
    
    # Conflicts:
    #	e2e/_suites/fleet/fleet.go

---
+++ e2e/_suites/fleet/system_integration.go
@@ -0,0 +1,247 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Jeffail/gabs/v2"
+	"github.com/cenkalti/backoff/v4"
+	"github.com/cucumber/godog"
+	"github.com/elastic/e2e-testing/internal/common"
+	"github.com/elastic/e2e-testing/internal/deploy"
+	"github.com/elastic/e2e-testing/internal/kibana"
+	"github.com/elastic/e2e-testing/internal/utils"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+)
+
+func (fts *FleetTestSuite) getAgentOSData() (string, error) {
+	agentService := deploy.NewServiceRequest(common.ElasticAgentServiceName)
+	manifest, _ := fts.getDeployer().Inspect(fts.currentContext, agentService)
+	agent, err := fts.kibanaClient.GetAgentByHostnameFromList(fts.currentContext, manifest.Hostname)
+	if err != nil {
+		return "", err
+	}
+	return agent.LocalMetadata.OS.Platform, nil
+}
+
+func (fts *FleetTestSuite) theMetricsInTheDataStream(name string, set string) error {
+	timeNow := time.Now()
+	startTime := timeNow.Unix()
+
+	maxTimeout := time.Duration(utils.TimeoutFactor) * time.Minute * 2
+	retryCount := 1
+
+	exp := utils.GetExponentialBackOff(maxTimeout)
+
+	os, _ := fts.getAgentOSData()
+
+	waitForDataStreams := func() error {
+		dataStreams, _ := fts.kibanaClient.GetDataStreams(fts.currentContext)
+
+		for _, item := range dataStreams.Children() {
+			if item.Path("dataset").Data().(string) == "system."+set {
+				log.WithFields(log.Fields{
+					"dataset":     "system." + set,
+					"elapsedTime": exp.GetElapsedTime(),
+					"enabled":     "true",
+					"retries":     retryCount,
+					"type":        name,
+					"os":          os,
+				}).Info("The " + name + " with value system." + set + " in the metrics")
+
+				if int64(int64(item.Path("last_activity_ms").Data().(float64))) > startTime {
+					log.WithFields(log.Fields{
+						"elapsedTime":      exp.GetElapsedTime(),
+						"last_activity_ms": item.Path("last_activity_ms").Data().(float64),
+						"retries":          retryCount,
+						"startTime":        startTime,
+						"os":               os,
+					}).Info("The " + name + " with value system." + set + " in the metrics")
+				}
+
+				return nil
+			}
+		}
+
+		err := errors.New("No " + name + " with value system." + set + " found in the metrics")
+
+		log.WithFields(log.Fields{
+			"elapsedTime": exp.GetElapsedTime(),
+			"name":        name,
+			"retry":       retryCount,
+			"set":         set,
+		}).Warn(err.Error())
+
+		retryCount++
+
+		return err
+	}
+
+	err := backoff.Retry(waitForDataStreams, exp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (fts *FleetTestSuite) thePolicyIsUpdatedToHaveSystemSet(name string, set string) error {
+	if name != "linux/metrics" && name != "system/metrics" && name != "logfile" && name != "log" {
+		log.WithFields(log.Fields{
+			"name": name,
+		}).Warn("We only support system system/metrics, log, logfile and linux/metrics policy to be updated")
+		return godog.ErrPending
+	}
+
+	var err error
+	var packageDS kibana.PackageDataStream
+	var kibanaInputs []kibana.Input
+	var metrics = ""
+
+	if name == "linux/metrics" {
+		metrics = "linux"
+		packageDS, err = fts.kibanaClient.GetIntegrationFromAgentPolicy(fts.currentContext, metrics, fts.Policy)
+		if err != nil {
+			return err
+		}
+
+		kibanaInputs = metricsInputs(name, set, "/linux_metrics.json", metrics)
+	} else if name == "system/metrics" || name == "logfile" || name == "log" {
+		metrics = "system"
+		packageDS, err = fts.kibanaClient.GetIntegrationFromAgentPolicy(fts.currentContext, metrics, fts.Policy)
+		if err != nil {
+			return err
+		}
+
+		packagePolicy, errPolicy := fts.kibanaClient.GetPackagePolicy(fts.currentContext, packageDS.ID)
+		if errPolicy != nil {
+			return errPolicy
+		}
+
+		kibanaInputs = packagePolicy.Inputs
+		log.WithFields(log.Fields{
+			"inputs": packagePolicy.Inputs,
+		}).Trace("Inputs from the package policy")
+	} else {
+		log.WithFields(log.Fields{
+			"type":    name,
+			"dataset": set,
+		}).Warn("Package Policy not supported yet")
+		return godog.ErrPending
+	}
+
+	os, _ := fts.getAgentOSData()
+
+	fts.Integration = packageDS.Package
+
+	log.WithFields(log.Fields{
+		"type":    name,
+		"dataset": metrics + "." + set,
+	}).Info("Getting information about Policy package type " + name + " name with dataset " + metrics + "." + set)
+
+	for _, item := range packageDS.Inputs {
+		if item.Type == name {
+			packageDS.Inputs = kibanaInputs
+		}
+	}
+	log.WithFields(log.Fields{
+		"inputs": packageDS.Inputs,
+	}).Info("Updating integration package config")
+
+	updatedAt, err := fts.kibanaClient.UpdateIntegrationPackagePolicy(fts.currentContext, packageDS)
+	if err != nil {
+		return err
+	}
+
+	fts.PolicyUpdatedAt = updatedAt
+
+	log.WithFields(log.Fields{
+		"dataset": metrics + "." + set,
+		"enabled": "true",
+		"type":    "metrics",
+		"os":      os,
+	}).Info("Policy Updated with package name " + metrics + "." + set)
+
+	return nil
+}
+
+func metricsInputs(integration string, set string, file string, metrics string) []kibana.Input {
+	metricsFile := filepath.Join(testResourcesDir, file)
+	jsonData, err := readJSONFile(metricsFile)
+	if err != nil {
+		log.Warnf("An error happened while reading metrics file, returning an empty array of inputs: %v", err)
+		return []kibana.Input{}
+	}
+
+	data := parseJSONMetrics(jsonData, integration, set, metrics)
+	return []kibana.Input{
+		{
+			Type:    integration,
+			Enabled: true,
+			Streams: data,
+		},
+	}
+
+	return []kibana.Input{}
+}
+
+func parseJSONMetrics(data *gabs.Container, integration string, set string, metrics string) []kibana.Stream {
+	for i, item := range data.Children() {
+		if item.Path("type").Data().(string) == integration {
+			for idx, stream := range item.S("streams").Children() {
+				dataSet, _ := stream.Path("data_stream.dataset").Data().(string)
+				if dataSet == metrics+"."+set {
+					data.SetP(
+						integration+"-"+metrics+"."+set+"-"+uuid.New().String(),
+						fmt.Sprintf("inputs.%d.streams.%d.id", i, idx),
+					)
+					data.SetP(
+						true,
+						fmt.Sprintf("inputs.%d.streams.%d.enabled", i, idx),
+					)
+
+					var dataStreamOut []kibana.Stream
+					if err := json.Unmarshal(data.Path(fmt.Sprintf("inputs.%d.streams", i)).Bytes(), &dataStreamOut); err != nil {
+						return []kibana.Stream{}
+					}
+
+					return dataStreamOut
+				}
+			}
+		}
+	}
+	return []kibana.Stream{}
+}
+
+func readJSONFile(file string) (*gabs.Container, error) {
+	jsonFile, err := os.Open(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.WithFields(log.Fields{
+		"file": file,
+	}).Info("Successfully Opened " + file)
+
+	defer jsonFile.Close()
+	data, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	jsonParsed, err := gabs.ParseJSON(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonParsed.S("inputs"), nil
+}
