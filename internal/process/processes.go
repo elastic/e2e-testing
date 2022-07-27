016dea44782ab11a5fec83910306be1c60d28f5b
Manuel de la Peña <mdelapenya@gmail.com>
2022-07-27 13:12:52 +0200

    chore: decouple responsibilities for fleet test suite code (#2850)
    * chore: move endpoint integration methods to a file
    
    * chore: move system integration methods to a file
    
    * chore: move stand-alone methods to its file
    
    * chore: move process-related methods to its file
    
    * chore: extract process check to its own internal package
    
    * chore: move top-level struct to remove file
    
    * fixup processes
    
    * chore: remove ingest_manager struct
    
    It causes confusion adding a new layer of indirection
    
    * chore: centralise adding steps to godog's scenario context
    
    * chore: rename main test file
    
    * chore: move before/after methods to the test file
    
    * chore: before/after methods are not from the struct
    
    * chore: extract upgrade steps to its file
    
    * chore: extract preconfigured_policy steps to its file
    
    * chore: extract integrations methods to a file
    
    * chore: simplify method for managing integrations
    
    * chore: extract more integrations methods
    
    * chore: extract more upgrade methods
    
    * chore: update pre-commit hooks
    
    * chore: extract api keys methods to its own file
    
    * chore: extract methods for agent enroll to its own file
    
    * chore: extract methods for agent deployment to its own file
    
    * chore: extract methods for agent checks to its own file
    
    * chore: extract methods for metrics/inputs to integrations files
    
    * fix: do not wait for kibana when the token is revoked
    
    Given https://github.com/elastic/kibana/issues/105078 is fixed, no need to wait for it anymore
    
    * chore: move struct and methods to test file
    
    * Revert "fix: do not wait for kibana when the token is revoked"
    
    This reverts commit c147114d2c866c8418bbd5b89fe2d01343698377.
    
    * chore: extract constants to their own files
    
    * Revert "chore: move struct and methods to test file"
    
    This reverts commit 2cf733d614df37d725b5252c73c67c2f8098bcc4.
    
    * chore: remove unused methods
    
    * chore: move search agent data to agent checks file
    
    * chore: move bootstrapFleet method to the test file
    
    * chore: rename initialiser methods
    
    * fix: rename test suite at Godog apis
    
    (cherry picked from commit fb0e134b7e6868a28e11846568b2da5180556fb5)

--- internal/process/processes.go
+++ internal/process/processes.go
@@ -2,58 +2,24 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-package main
+package process
 
 import (
 	"context"
 	"fmt"
 	"runtime"
-	"strconv"
 	"time"
 
 	"github.com/elastic/e2e-testing/internal/action"
-	"github.com/elastic/e2e-testing/internal/common"
 	"github.com/elastic/e2e-testing/internal/deploy"
 	"github.com/elastic/e2e-testing/internal/utils"
 	log "github.com/sirupsen/logrus"
 )
 
-// IngestManagerTestSuite represents a test suite, holding references to the pieces needed to run the tests
-type IngestManagerTestSuite struct {
-	Fleet *FleetTestSuite
-}
-
-func (imts *IngestManagerTestSuite) processStateOnTheHost(process string, state string) error {
-	ocurrences := "1"
-	if state == "uninstalled" || state == "stopped" {
-		ocurrences = "0"
-	}
-	return imts.thereAreInstancesOfTheProcessInTheState(ocurrences, process, state)
-}
-
-func (imts *IngestManagerTestSuite) thereAreInstancesOfTheProcessInTheState(ocurrences string, process string, state string) error {
-	agentService := deploy.NewServiceRequest(common.ElasticAgentServiceName)
-	manifest, _ := imts.Fleet.deployer.Inspect(imts.Fleet.currentContext, agentService)
-
-	count, err := strconv.Atoi(ocurrences)
-	if err != nil {
-		return err
-	}
-
-	var srv deploy.ServiceRequest
-	if imts.Fleet.StandAlone {
-		srv = deploy.NewServiceContainerRequest(manifest.Name)
-	} else {
-		srv = deploy.NewServiceRequest(manifest.Name)
-	}
-
-	return CheckProcessState(imts.Fleet.currentContext, imts.Fleet.deployer, srv, process, state, count)
-}
-
-// CheckProcessState checks if a process is in the desired state in a container
+// CheckState checks if a process is in the desired state in a container
 // name of the container for the service:
 // we are using the underlying deployer to run the commands in the container/service
-func CheckProcessState(ctx context.Context, deployer deploy.Deployment, service deploy.ServiceRequest, process string, state string, occurrences int) error {
+func CheckState(ctx context.Context, deployer deploy.Deployment, service deploy.ServiceRequest, process string, state string, occurrences int) error {
 	timeout := time.Duration(utils.TimeoutFactor) * time.Minute
 
 	if runtime.GOOS == "windows" {
