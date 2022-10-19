fda3805afc447f0e39c0766fe62cd81248930f0b
apmmachine <58790750+apmmachine@users.noreply.github.com>
2022-10-19 02:18:42 -0400

    bump stack version 8.6.0-6545f2df (#3135)
    Co-authored-by: apmmachine <infra-root-apmmachine@elastic.co>

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -36,7 +36,7 @@ const FleetProfileName = "fleet"
 const FleetServerAgentServiceName = "fleet-server"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = "8.6.0-cae815eb-SNAPSHOT"
+var BeatVersionBase = "8.6.0-6545f2df-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
