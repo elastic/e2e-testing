2b235f1b4c0eee5977e5d4ad1d5ba714f5b9c77a
Victor Martinez <victormartinezrubio@gmail.com>
2023-03-08 22:01:27 +0100

    Update internal/common/defaults.go

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -36,7 +36,7 @@ const FleetProfileName = "fleet"
 const FleetServerAgentServiceName = "fleet-server"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = = "8.8.0-3648efbb-SNAPSHOT"
+var BeatVersionBase = "8.8.0-3648efbb-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
