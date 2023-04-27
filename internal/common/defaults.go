3d8ca180a6cc44aa73ad565a8686c08de75a7f9d
apmmachine <58790750+apmmachine@users.noreply.github.com>
2023-04-27 12:07:41 -0400

    [updatecli] update elastic stack version for testing 8.6.3-153a82ef (#3477)
    * chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    
    ... /e2e-testing/internal/config/compose/services/metricbeat/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/profiles/fleet/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/elastic-agent/cloud/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/elastic-agent/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/elastic-agent/fleet-server/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/elasticsearch/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/kibana/docker-compose.yml"
    
    Made with ❤️️ by updatecli
    
    * chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    
    ... /e2e-testing/.stack-version"
    
    Made with ❤️️ by updatecli
    
    * chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    
    ... /e2e-testing/internal/common/defaults.go"
    
    Made with ❤️️ by updatecli
    
    * chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    
    ... /e2e-testing/.ci/Jenkinsfile"
    
    Made with ❤️️ by updatecli
    
    * chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    
    ... /e2e-testing/internal/config/kubernetes/base/fleet-server/deployment.yaml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/kubernetes/base/elasticsearch/deployment.yaml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/kubernetes/base/kibana/deployment.yaml"
    
    Made with ❤️️ by updatecli
    
    ---------
    
    Co-authored-by: apmmachine <apmmachine@users.noreply.github.com>

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -36,7 +36,7 @@ const FleetProfileName = "fleet"
 const FleetServerAgentServiceName = "fleet-server"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = "8.6.3-a519b953-SNAPSHOT"
+var BeatVersionBase = "8.6.3-153a82ef-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
