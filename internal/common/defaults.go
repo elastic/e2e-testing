b004bed429561045e2a786d5fec396ca50b5abfe
apmmachine <apmmachine@users.noreply.github.com>
2023-06-05 15:02:26 +0000

    chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    ... /e2e-testing/internal/common/defaults.go"
    
    Made with ❤️️ by updatecli

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -36,7 +36,7 @@ const FleetProfileName = "fleet"
 const FleetServerAgentServiceName = "fleet-server"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = "8.8.1-afb32ff1-SNAPSHOT"
+var BeatVersionBase = "8.8.1-67054589-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
