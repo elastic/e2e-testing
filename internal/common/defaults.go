35d0f43ce642a94ecf02f3528cee774510565b59
apmmachine <58790750+apmmachine@users.noreply.github.com>
2023-06-13 13:24:49 -0400

    [updatecli] update elastic stack version for testing 8.9.0-37a365f9 (#3547)
    * chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    
    ... /e2e-testing/internal/common/defaults.go"
    
    Made with ❤️️ by updatecli
    
    * chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    
    ... /e2e-testing/.ci/Jenkinsfile"
    
    Made with ❤️️ by updatecli
    
    * chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    
    ... /e2e-testing/internal/config/kubernetes/base/fleet-server/deployment.yaml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/kubernetes/base/elasticsearch/deployment.yaml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/kubernetes/base/kibana/deployment.yaml"
    
    Made with ❤️️ by updatecli
    
    * chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    
    ... /e2e-testing/internal/config/compose/profiles/fleet/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/elastic-agent/cloud/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/elastic-agent/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/elastic-agent/fleet-server/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/elasticsearch/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/kibana/docker-compose.yml"
    Updated the content of the file "/tmp/updatecli/github/elastic/e2e-testing/internal/config/compose/services/metricbeat/docker-compose.yml"
    
    Made with ❤️️ by updatecli
    
    * chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    
    ... /e2e-testing/.stack-version"
    
    Made with ❤️️ by updatecli
    
    ---------
    
    Co-authored-by: apmmachine <apmmachine@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -36,7 +36,7 @@ const FleetProfileName = "fleet"
 const FleetServerAgentServiceName = "fleet-server"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = "8.9.0-f766627c-SNAPSHOT"
+var BeatVersionBase = "8.9.0-37a365f9-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
