41ea52fbdec9d11bcb8c17c91f16a35f85ec3d75
apmmachine <apmmachine@users.noreply.github.com>
2023-10-19 15:02:15 +0000

    chore: Update defaults.go
    Made with ❤️️ by updatecli

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -36,7 +36,7 @@ const FleetProfileName = "fleet"
 const FleetServerAgentServiceName = "fleet-server"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = "8.11.0-50f754c3-SNAPSHOT"
+var BeatVersionBase = "8.11.0-64ca6348-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
